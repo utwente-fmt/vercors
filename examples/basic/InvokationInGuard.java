33f86d2de6ed6875836ce6244fcbce1accc4008a
Pieter Bos <info@pieterbos.me>
2019-10-28 16:29:34 +0100

    fixes #189: flatten if statements and their conditions

---
+++ examples/basic/InvokationInGuard.java
@@ -0,0 +1,24 @@
+//:: cases InvokationInGuard
+//:: tools silicon
+//:: verdict Pass
+
+class InvokationInGuard {
+  int bar() { return 0; }
+  int foo() { return 0; }
+
+  void action(int i) {
+      return;
+  }
+
+  void test() {
+    if(bar() == bar()) {
+        action(0);
+    } else if(bar() == foo()) {
+        action(1);
+    } else if(foo() == bar()) {
+        action(2);
+    } else {
+        action(3);
+    }
+  }
+}
