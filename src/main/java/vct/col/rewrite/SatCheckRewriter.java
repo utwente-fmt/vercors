36017469d5dffa50c5b1db2cd12d360b1919a63d
bobismijnnaam <bobrubbens@gmail.com>
2019-11-04 16:55:53 +0100

    SatCheckRewriter now emits other methods with the contract of the original method containing an "assert false" to check for satisfiability

--- src/main/java/vct/col/rewrite/SatCheckRewriter.java
+++ src/main/java/vct/col/rewrite/SatCheckRewriter.java
@@ -1,10 +1,15 @@
 package vct.col.rewrite;
 
 import hre.ast.BranchOrigin;
+import hre.ast.Origin;
+import scala.reflect.internal.Trees;
 import vct.col.ast.generic.ASTNode;
-import vct.col.ast.stmt.decl.ASTSpecial;
+import vct.col.ast.generic.ASTSequence;
 import vct.col.ast.stmt.composite.BlockStatement;
+import vct.col.ast.stmt.decl.ASTSpecial;
 import vct.col.ast.stmt.decl.Contract;
+import vct.col.ast.type.PrimitiveSort;
+import vct.col.ast.type.PrimitiveType;
 import vct.col.ast.util.ContractBuilder;
 import vct.col.ast.stmt.decl.DeclarationStatement;
 import vct.col.ast.stmt.decl.Method;
@@ -13,6 +18,7 @@ import vct.col.ast.type.Type;
 import vct.col.util.OriginWrapper;
 
 public class SatCheckRewriter extends AbstractRewriter {
+    static int i = 0;
 
     public SatCheckRewriter(ProgramUnit source) {
         super(source);
@@ -20,40 +26,36 @@ public class SatCheckRewriter extends AbstractRewriter {
 
     @Override
     public void visit(Method m) {
-        //checkPermission(m);
-        String name = m.getName();
-        if (currentContractBuilder == null) {
-            currentContractBuilder = new ContractBuilder();
+        // Find out if contract is intentionally false
+        // Since if it is marked as false intentionally we do not have to check it
+        Contract contract = m.getContract();
+        boolean intentional_false = contract != null && contract.pre_condition.isConstant(false);;
+
+        // If the contract is not intentionally false and the method is not abstract,
+        // create a proof obligation that shows the contract is satisfiable
+        if (m.getBody() != null && !intentional_false && (m.kind == Method.Kind.Plain || m.kind == Method.Kind.Constructor)) {
+            // Create { assert false; } block
+            ASTNode my_assert = create.special(ASTSpecial.Kind.Assert, create.constant(false));
+            BranchOrigin my_branch = new BranchOrigin("contract satisfiability check", null);
+            OriginWrapper.wrap(null, my_assert, my_branch);
+            BlockStatement blockStatement = create.block(my_assert);
+
+            // Create an extra method containing it
+            Method assert_method = create.method_kind(
+                    m.getKind(),
+                    rewrite(m.getReturnType()),
+                    rewrite(m.getContract()),
+                    "__contract_unsatisfiable__" + m.name(),
+                    rewrite(m.getArgs()),
+                    m.usesVarArgs(),
+                    blockStatement
+            );
+
+            currentTargetClass.add(assert_method);
         }
-        DeclarationStatement args[] = rewrite(m.getArgs());
-        Contract mc = m.getContract();
-        boolean intentional_false = false;
-        if (mc != null) {
-            rewrite(mc, currentContractBuilder);
-            intentional_false = mc.pre_condition.isConstant(false);
-        }
-        Method.Kind kind = m.kind;
-        Type rt = rewrite(m.getReturnType());
-        Contract c = currentContractBuilder.getContract();
-        currentContractBuilder = null;
-        ASTNode body = rewrite(m.getBody());
-        if (body != null && !intentional_false) {
-            switch (kind) {
-                case Plain:
-                case Constructor:
-                    ASTNode refute = create.special(ASTSpecial.Kind.Refute, create.constant(false));
-                    BranchOrigin branch = new BranchOrigin("Contract Unsatisfiability Check", null);
-                    OriginWrapper.wrap(null, refute, branch);
-                    if (body instanceof BlockStatement) {
-                        ((BlockStatement) body).prepend(refute);
-                    } else {
-                        body = create.block(refute, body);
-                    }
-                default:
-                    break;
-            }
-        }
-        result = create.method_kind(kind, rt, c, name, args, m.usesVarArgs(), body);
+
+        // After visit, result should be equal to the rewritten version of m, i.e. nicely deep cloned
+        super.visit(m);
     }
 
 }
