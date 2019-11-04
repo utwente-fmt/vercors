ce85c9296daac44e59ab67e2973b01cdad86d4a7
bobismijnnaam <bobrubbens@gmail.com>
2019-11-04 12:34:12 +0100

    Started formatting SatChecKRewriter

--- src/main/java/vct/col/rewrite/SatCheckRewriter.java
+++ src/main/java/vct/col/rewrite/SatCheckRewriter.java
@@ -14,42 +14,46 @@ import vct.col.util.OriginWrapper;
 
 public class SatCheckRewriter extends AbstractRewriter {
 
-	public SatCheckRewriter(ProgramUnit source) {
-	  super(source);
-  }
+    public SatCheckRewriter(ProgramUnit source) {
+        super(source);
+    }
 
-  @Override
-  public void visit(Method m) {
-    //checkPermission(m);
-    String name=m.getName();
-    if (currentContractBuilder==null) currentContractBuilder=new ContractBuilder();
-    DeclarationStatement args[]=rewrite(m.getArgs());
-    Contract mc=m.getContract();
-    boolean intentional_false=false;
-    if (mc!=null){
-      rewrite(mc,currentContractBuilder);
-      intentional_false=mc.pre_condition.isConstant(false);
+    @Override
+    public void visit(Method m) {
+        //checkPermission(m);
+        String name = m.getName();
+        if (currentContractBuilder == null) {
+            currentContractBuilder = new ContractBuilder();
+        }
+        DeclarationStatement args[] = rewrite(m.getArgs());
+        Contract mc = m.getContract();
+        boolean intentional_false = false;
+        if (mc != null) {
+            rewrite(mc, currentContractBuilder);
+            intentional_false = mc.pre_condition.isConstant(false);
+        }
+        Method.Kind kind = m.kind;
+        Type rt = rewrite(m.getReturnType());
+        Contract c = currentContractBuilder.getContract();
+        currentContractBuilder = null;
+        ASTNode body = rewrite(m.getBody());
+        if (body != null && !intentional_false) {
+            switch (kind) {
+                case Plain:
+                case Constructor:
+                    ASTNode refute = create.special(ASTSpecial.Kind.Refute, create.constant(false));
+                    BranchOrigin branch = new BranchOrigin("Contract Unsatisfiability Check", null);
+                    OriginWrapper.wrap(null, refute, branch);
+                    if (body instanceof BlockStatement) {
+                        ((BlockStatement) body).prepend(refute);
+                    } else {
+                        body = create.block(refute, body);
+                    }
+                default:
+                    break;
+            }
+        }
+        result = create.method_kind(kind, rt, c, name, args, m.usesVarArgs(), body);
     }
-    Method.Kind kind=m.kind;
-    Type rt=rewrite(m.getReturnType());
-    Contract c=currentContractBuilder.getContract();
-    currentContractBuilder=null;
-    ASTNode body=rewrite(m.getBody());
-    if (body!=null && !intentional_false) switch(kind){
-    case Plain:
-    case Constructor:
-      ASTNode refute=create.special(ASTSpecial.Kind.Refute,create.constant(false));
-    	BranchOrigin branch=new BranchOrigin("Contract Unsatisfiability Check",null);
-      OriginWrapper.wrap(null,refute, branch);
-	    if (body instanceof BlockStatement){
-	    	((BlockStatement)body).prepend(refute);
-	    } else {
-	    	body=create.block(refute,body);
-	    }
-    default:
-      break;
-    }
-    result=create.method_kind(kind, rt, c, name, args, m.usesVarArgs(), body);
-  }
 
 }
