a7f12b565b9fed3c3687aa2832d5286dffcdd25a
bobismijnnaam <bobrubbens@gmail.com>
2020-05-05 16:30:40 +0200

    Add the key abrupt termination challenge and a test for throws and throw.

---
+++ examples/abrupt/KeYAbruptTerminationChallenge.java
@@ -0,0 +1,38 @@
+// -*- tab-width:4 ; indent-tabs-mode:nil -*-
+//:: cases KeYAbruptTerminationChallenge
+//:: tools silicon
+//:: verdict Pass
+
+// This is an adaptation of a file from the examples directory of key.
+// Original file path: KEY_ROOT/key/key.ui/examples/standard_key/challenges/jacobsEtAl/abruptTermination/AbruptTermination.java
+// It has been adapted to use the vercors syntax for specifications.
+
+class AbruptTermination {
+    int[] ia;
+
+    //@ context_everywhere Perm(ia, read) ** ia != null;
+    //@ context_everywhere (\forall* int i; 0 <= i && i < ia.length; Perm(ia[i], write));
+    //@ ensures ia == \old(ia); // Extra contract needed for VerCors
+    // ensure i is the first position with negative value
+    /*@ ensures (\forall int i; 0 <= i && i < ia.length;
+                    (\old(ia[i]) < 0 && (\forall int j; 0 <= j && j < i; \old(ia[j]) >= 0))
+                        ? (ia[i] == -\old(ia[i]))
+                        : (ia[i] == \old(ia[i]))
+                );
+    @*/
+    void negatefirst() {
+        /*@
+            loop_invariant 0 <= i && i <= ia.length;
+            loop_invariant (\forall int j; 0<=j && j<i; ia[j] >= 0 && ia[j] == \old(ia[j]));
+            // Extra invariants needed for VerCors
+            loop_invariant (\forall int j; i<=j && j<ia.length; ia[j] == \old(ia[j]));
+            loop_invariant ia == \old(ia);
+         */
+        for (int i = 0 ; i < ia.length; i++) {
+            if (ia[i] < 0) {
+                ia[i] = -ia[i] ;
+                break ;
+            }
+        }
+    }
+}
