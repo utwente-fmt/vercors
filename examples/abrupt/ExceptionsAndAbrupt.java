77cb69651b35a4e37488923d8eb4e8fdbbefa116
bobismijnnaam <bobrubbens@gmail.com>
2020-05-06 15:30:17 +0200

    Further refine tests.

--- examples/abrupt/ExceptionsAndAbrupt.java
+++ examples/abrupt/ExceptionsAndAbrupt.java
@@ -7,40 +7,45 @@
 // Original file path: KEY_ROOT/key/key.ui/examples/standard_key/java_dl/exceptions_java/MyClass2.java
 // It has been adapted to use the vercors syntax for specifications.
 
-public class MyClass {
-    //@ ensures i == 0 ==> \result == i + 3;
-	//@ ensures i != 0 ==> \result == i + 2;
-	public void blah(int i) {
-		l1: {
-			try{
-				if (i==0) {
-					break l1;
-				}
+public class C {
+    //@ ensures \old(input_i) == 0 ==> \result == \old(input_i) + 2;
+    //@ ensures \old(input_i) != 0 ==> \result == \old(input_i) + 3;
+    public int foo(int input_i) {
+        int i = input_i;
+        l1: {
+            try{
+                if (i==0) {
+                    break l1;
+                }
+                i = i + 1;
+            } catch (Exception e) {
+                // Nothing
+                //@ assert false; // Is not executed
+            } finally{
+                i = i + 1;
+            }
+        }
 
-				i = i + 1;
-			} catch (Exception e) {
-				// Nothing
-			} finally{
-				i = i + 1;
-			}
-		}
+        i = i + 1;
 
-		i = i + 1;
+        return i;
+    }
 
-		return i;
-	}
-
-	//@ ensures \result == 2;
-	public int m() {
-		IllegalArgumentException e = new IllegalArgumentException();
-		try {
-			throw e;
-		} catch (IllegalStateException e0) {
-			return 0;
-		} catch (RuntimeException e1) {
-			return 1;
-		} finally {
-			return 2;
-		}
-	}
+    //@ ensures \result == 2;
+    public int bar() {
+        IllegalArgumentException e = new IllegalArgumentException();
+        int i = 0;
+        try {
+            throw e;
+        } catch (IllegalStateException e0) {
+            //@ assert false; // Should never happen
+            return 0;
+        } catch (RuntimeException e1) {
+            i = 10;
+            return 1;
+        } finally {
+            //@ assert i == 10; // Went through the RunTimeException first
+            return 2;
+        }
+    }
 }
--- examples/abrupt/WhileFinally.java
+++ examples/abrupt/WhileFinally.java
@@ -7,23 +7,15 @@ class Java {
     boolean p();
 
     int foo() {
-        boolean ppp = p();
-        my_if: if (ppp) {
-            break my_if;
-        }
-
         int x = 0;
         boolean pp = p();
-        //@ loop_invariant x == 0 || x == 4;
+        //@ loop_invariant x == 0;
         while (pp) {
             try {
                 try {
-                    //@ assert x == 0 || x == 4;
+                    //@ assert x == 0;
                     x = 1;
                     break;
-                    x = 2;
-                    continue;
-                    return 3;
                 } finally {
                     //@ assert x == 1;
                     x = 3;
@@ -33,6 +25,6 @@ class Java {
                 x = 4;
             }
         }
-        //@ assert x == 0 || x == 4;
+        //@ assert (!(x == 0) && (x == 4)) || ((x == 0) && !(x == 4));
     }
 }
