2f4863489e4a6155bd826386a0090326dc8d5fc6
Bob Rubbens <bobrubbens@gmail.com>
2020-07-03 15:50:37 +0200

    Add tests for semantics of signals

--- examples/abrupt/TypecheckSignalsOk.java
+++ examples/abrupt/TypecheckSignalsOk.java
@@ -3,8 +3,30 @@
 //:: tools silicon
 //:: verdict Pass
 
+import java.io.*;
+
 final class Ok {
     //@ signals (RuntimeException e) true;
     void m1();
+
+    int x;
+
+    void m2() {
+        try {
+            m3();
+        } catch (RuntimeException e) {
+            assert x == 3;
+        }
+    }
+
+    //@ signals (RuntimeException e) Perm(x, read) ** x == 3;
+    void m3();
+
+    //@ signals (RuntimeException e) false;
+    void m4();
+
+    void m5() {
+        m4();
+    }
 }
 
