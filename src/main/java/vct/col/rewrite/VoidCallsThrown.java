95747321faa72a68da18181f67d0c19566e5f912
Bob Rubbens <bobrubbens@gmail.com>
2020-07-07 10:25:37 +0200

    Replace Abort with requireNonNull in VoidCallsThrown to test SonarCloud

--- src/main/java/vct/col/rewrite/VoidCallsThrown.java
+++ src/main/java/vct/col/rewrite/VoidCallsThrown.java
@@ -1,5 +1,6 @@
 package vct.col.rewrite;
 
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import vct.col.ast.stmt.composite.IfStatement;
@@ -129,7 +130,8 @@ public class VoidCallsThrown extends AbstractRewriter {
   
   public void visit(MethodInvokation e){
     Method m=e.getDefinition();
-    if (m==null) Abort("unexpected null method definition at %s",e.getOrigin());
+    Objects.requireNonNull(m, "Method definition required");
+//    if (m==null) Abort("unexpected null method definition at %s",e.getOrigin());
     switch(m.kind){
     case Predicate:
     case Pure:
