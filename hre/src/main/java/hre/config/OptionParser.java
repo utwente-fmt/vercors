fbdccb68adfe988e836b6a06a053811c17a6f3af
Bob Rubbens <bobrubbens@gmail.com>
2020-07-08 15:58:15 +0200

    Fix sonarcloud bug in OptionParser

--- hre/src/main/java/hre/config/OptionParser.java
+++ hre/src/main/java/hre/config/OptionParser.java
@@ -1,9 +1,6 @@
 package hre.config;
 
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
 
 import static hre.lang.System.*;
@@ -74,9 +71,7 @@ public class OptionParser {
           name=name.substring(0,arg_idx);
         }
         Option opt=long_options.get(name);
-        if (opt==null){
-          Fail("unknown option %s",name);
-        }
+        Objects.requireNonNull(opt, String.format("unknown option %s", name));
         if (arg==null && opt.needsArgument() && i+1<args.length){
           i++;
           arg=args[i];
@@ -104,9 +99,7 @@ public class OptionParser {
         }
         for(int j=1;j<N;j++){
           Option opt=short_options.get(args[i].charAt(j));
-          if (opt==null){
-            Fail("unknown flag %c",args[i].charAt(j));
-          }
+          Objects.requireNonNull(opt, String.format("unknown flag %c", args[i].charAt(j)));
           if (opt.needsArgument()){
             Fail("option %c needs an argument.",args[i].charAt(j));
           }
