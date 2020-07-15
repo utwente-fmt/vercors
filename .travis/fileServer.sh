cf47820a65b8d560825dcac047e129a29ba543b8
Bob Rubbens <bobrubbens@gmail.com>
2020-07-15 15:33:04 +0200

    Change paths again

--- .travis/fileServer.sh
+++ .travis/fileServer.sh
@@ -5,15 +5,17 @@ export VERCORS_FILE_SERVER_PORT="873"
 export VERCORS_FILE_SERVER_ADDR="${VERCORS_FILE_SERVER_IP}:${VERCORS_FILE_SERVER_PORT}"
 
 uploadBuildData() {
+  # Use \* to prevent * from expanding to dir contents. Instead, we just want to pass the literal with * to rsync
+  # Otherwise the one file in the directory is uploaded as the file ${TRAVIS_BUILD_NUMBER}, which instead should become a folder
   RSYNC_PASSWORD="${VERCORS_RSYNC_PASSWORD}" rsync -avP --no-perms --no-owner --no-group \
-    ./sync/${TRAVIS_BUILD_NUMBER}/* \
+    ./sync/${TRAVIS_BUILD_NUMBER}/\* \
     rsync://travis@${VERCORS_FILE_SERVER_ADDR}/volume/${TRAVIS_BUILD_NUMBER}
 }
 
 downloadBuildData() {
   mkdir -p sync/${TRAVIS_BUILD_NUMBER}
   RSYNC_PASSWORD="${VERCORS_RSYNC_PASSWORD}" rsync -avP --no-perms --no-owner --no-group \
-    rsync://travis@${VERCORS_FILE_SERVER_ADDR}/volume/${TRAVIS_BUILD_NUMBER}/*
+    rsync://travis@${VERCORS_FILE_SERVER_ADDR}/volume/${TRAVIS_BUILD_NUMBER}/* \
     sync/${TRAVIS_BUILD_NUMBER}
 }
 
@@ -22,7 +24,7 @@ clearBuildData() {
   rm -r sync/${TRAVIS_BUILD_NUMBER}
   mkdir -p sync/${TRAVIS_BUILD_NUMBER}
   RSYNC_PASSWORD="${VERCORS_RSYNC_PASSWORD}" rsync -avP --no-perms --no-owner --no-group --delete \
-    ./sync/${TRAVIS_BUILD_NUMBER}/* \
+    ./sync/${TRAVIS_BUILD_NUMBER}/\* \
     rsync://travis@${VERCORS_FILE_SERVER_ADDR}/volume/${TRAVIS_BUILD_NUMBER}
 }
 
