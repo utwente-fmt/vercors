df502e61c8022585b11f6d47594e00ee70a83a2e
Bob Rubbens <bobrubbens@gmail.com>
2020-07-15 16:20:54 +0200

    Streamline extra installed dependencies

--- .travis/decrypt-vercors-scratch-ci-key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Decrypt file
-openssl aes-256-cbc -K $encrypted_d2cc1ee25cc3_key -iv $encrypted_d2cc1ee25cc3_iv -in $TRAVIS_BUILD_DIR/vercors_buildbot.enc -out $TRAVIS_BUILD_DIR/vercors_buildbot -d
-# Set appropriate permissions for use as ssh identity file
-chmod 600 $TRAVIS_BUILD_DIR/vercors_buildbot
