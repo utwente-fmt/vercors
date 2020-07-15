b2b1410bf6fba1c958f407c613a53e69e41d0671
Bob Rubbens <bobrubbens@gmail.com>
2020-07-15 15:09:54 +0200

    Start doing coverage

--- .travis/build.sh
+++ .travis/build.sh
@@ -7,4 +7,8 @@ tree sync
 
 ./.travis/travis_fold.sh build "Building VerCors" "./.travis/20-build.sh"
 ./.travis/travis_fold.sh fibonacci "Checking manual example" "./bin/vct --silicon $TRAVIS_BUILD_DIR/examples/manual/fibonacci.pvl"
-./.travis/travis_fold.sh test "Executing tests" "./bin/vct --progress --test=$TRAVIS_BUILD_DIR/examples --travis-test-output --test-workers=1 --tool=silicon --exclude-suite=slow,medium,problem-fail,skip-travis"
+./.travis/travis_fold.sh test "Executing tests" "./bin/vct --progress --test=$TRAVIS_BUILD_DIR/examples --travis-test-output --test-workers=1 --tool=silicon --exclude-suite=slow,medium,problem-fail,skip-travis --enable-test-coverage --coverage-output-file=sync/${TRAVIS_BUILD_NUMBER}/${TRAVIS_JOB_NUMBER}.xml"
+
+tree sync
+.travis/travis_fold.sh uploadBuildData "Uploading coverage .xml file" uploadBuildData
+tree sync
