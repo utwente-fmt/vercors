a12ac78f36ebea6c3b51ebcb09eaec7f5d9e3688
Bob Rubbens <bobrubbens@gmail.com>
2020-07-16 16:10:44 +0200

    Remove last comments and rename build.sh to runTests.sh

--- .travis/fileServer.sh
+++ .travis/fileServer.sh
@@ -5,8 +5,6 @@ export VERCORS_FILE_SERVER_PORT="873"
 export VERCORS_FILE_SERVER_ADDR="${VERCORS_FILE_SERVER_IP}:${VERCORS_FILE_SERVER_PORT}"
 
 uploadBuildData() {
-  # Use \* to prevent * from expanding to dir contents. Instead, we just want to pass the literal with * to rsync
-  # Otherwise the one file in the directory is uploaded as the file ${TRAVIS_BUILD_NUMBER}, which instead should become a folder
   RSYNC_PASSWORD="${VERCORS_RSYNC_PASSWORD}" rsync -avP --no-perms --no-owner --no-group \
     sync/${TRAVIS_BUILD_NUMBER} \
     rsync://travis@${VERCORS_FILE_SERVER_ADDR}/volume
