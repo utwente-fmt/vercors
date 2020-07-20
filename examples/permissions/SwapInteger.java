b411d50d04a6e8374ce3f38938da3905722f20e5
Sophie Lathouwers <sophie.lathouwers@gmail.com>
2020-07-20 11:43:57 +0200

    Fixed several Java examples, I skipped those that used witnesses for now

--- examples/permissions/SwapInteger.java
+++ examples/permissions/SwapInteger.java
@@ -1,16 +1,18 @@
 // -*- tab-width:2 ; indent-tabs-mode:nil -*-
 //:: cases SwapInteger
-//:: tools chalice
-//:: verdict Fail
+//:: tools silicon
+//:: verdict Pass SwapInteger.n SwapInteger.twice
+//:: verdict Fail SwapInteger.wrong
 
 public class SwapInteger {
 
   int F;
   int G;
-  /*@ requires Perm(F,100) ** Perm(G,100);
-      ensures Perm(F,100) ** Perm(G,100);
-      ensures F == \old(G) && G == \old(F);
-   */
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(G) && G == \old(F);
+  @*/
   void n()
   {
     int tmp;
@@ -20,20 +22,22 @@ public class SwapInteger {
   }
 
 
-  /*@ requires Perm(F,100) ** Perm(G,100);
-      ensures Perm(F,100) ** Perm(G,100);
-      ensures F == \old(F) && G == \old(G);
-   */
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(F) && G == \old(G);
+  @*/
   void twice()
   {
     n();
     n();
   }
   
-  /*@ requires Perm(F,100) ** Perm(G,100);
-      ensures Perm(F,100) ** Perm(G,100);
-      ensures F == \old(F) && G == \old(G);
-   */
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(F) && G == \old(G);
+  @*/
   void wrong()
   {
     n();
--- examples/permissions/SwapLong.java
+++ examples/permissions/SwapLong.java
@@ -1,16 +1,21 @@
 // -*- tab-width:2 ; indent-tabs-mode:nil -*-
 //:: cases SwapLong
-//:: tools chalice
-//:: verdict Fail
+//:: suite skip-travis
+// tools silicon
+// verdict Pass SwapLong.n SwapLong.twice
+// verdict Fail SwapLong.wrong
+
+// Issue with Long things in VerCors
 
 public class SwapLong {
 
   long F;
   long G;
-  /*@ requires Perm(F,100) ** Perm(G,100);
-      ensures Perm(F,100) ** Perm(G,100);
-      ensures F == \old(G) && G == \old(F);
-   */
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(G) && G == \old(F);
+  @*/
   void n()
   {
     long tmp;
@@ -20,20 +25,22 @@ public class SwapLong {
   }
 
 
-  /*@ requires Perm(F,100) ** Perm(G,100);
-      ensures Perm(F,100) ** Perm(G,100);
-      ensures F == \old(F) && G == \old(G);
-   */
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(F) && G == \old(G);
+  @*/
   void twice()
   {
     n();
     n();
   }
   
-  /*@ requires Perm(F,100) ** Perm(G,100);
-      ensures Perm(F,100) ** Perm(G,100);
-      ensures F == \old(F) && G == \old(G);
-   */
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(F) && G == \old(G);
+  @*/
   void wrong()
   {
     n();
