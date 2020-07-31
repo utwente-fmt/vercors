0b5f28455cf69707d48fab2c39661be46c599e69
Bob Rubbens <bobrubbens@gmail.com>
2020-07-31 14:40:42 +0200

    Refactor LocalVariableChecker to have less nesting

--- src/main/java/vct/col/util/LocalVariableChecker.scala
+++ src/main/java/vct/col/util/LocalVariableChecker.scala
@@ -19,12 +19,11 @@ object LocalVariableChecker {
   * - Vars used in invariants must be effectively final
   */
 class LocalVariableChecker(arg: ProgramUnit) extends RecursiveVisitor[Unit](arg) {
+  val accessSets: mutable.Map[ASTNode, Set[String]] = mutable.Map[ASTNode, Set[String]]()
+  val writeSets: mutable.Map[ASTNode, Set[String]] = mutable.Map[ASTNode, Set[String]]()
+
   override def visit(region: ParallelRegion): Unit = {
-    // Check that: all may read, or one may write
-
-    val accessSets = mutable.Map[ASTNode, Set[String]]()
-    val writeSets = mutable.Map[ASTNode, Set[String]]()
-
+    // Pre-calculate access and write sets
     for (block <- region.blocks) {
       val ns = new NameScanner
       block.accept(ns)
@@ -32,24 +31,27 @@ class LocalVariableChecker(arg: ProgramUnit) extends RecursiveVisitor[Unit](arg)
       writeSets.put(block, ns.writes)
     }
 
-    for (parallelBlockA <- region.blocks) {
-      if (parallelBlockA.iters.isEmpty) {
-        // If a parallel block models 1 thread, it may write to stack variables, provided other threads
-        // do not access (read/write) it
-        for (parallelBlockB <- region.blocks.filter(_ ne parallelBlockA)) {
-          val intersection = writeSets(parallelBlockA).intersect(accessSets(parallelBlockB))
-          if (intersection.nonEmpty) {
-            Abort("Parallelblock writes to the following shared variables: %s", intersection.mkString(", "))
-          }
+    region.blocks.foreach(checkParallelBlock(_, region.blocks))
+  }
+
+  def checkParallelBlock(parallelBlock: ParallelBlock, neighbours: List[ParallelBlock]): Unit = {
+    if (parallelBlock.iters.isEmpty) {
+      // If a parallel block models 1 thread, it may write to stack variables, provided other threads
+      // do not access (read/write) it. Implicitly, only one par block can have write permission for a stack var.
+      // Conversely, every par block can get a read permission for any var, if none of the par blocks write.
+      for (parallelBlockB <- neighbours.filter(_ ne parallelBlock)) {
+        val intersection = writeSets(parallelBlock).intersect(accessSets(parallelBlockB))
+        if (intersection.nonEmpty) {
+          Abort("Parallelblock writes to the following shared variables: %s", intersection.mkString(", "))
         }
-      } else {
-        // If a parallel block models 1 or more threads, it may not write to stack variables, as this
-        // could potentially cause data races between threads
-        if (writeSets(parallelBlockA).nonEmpty) {
-          val msg = "Parallel block modelling multiple threads cannot write to shared stack variables"
-          parallelBlockA.getOrigin.report("error", msg)
-          Abort("")
-        }
+      }
+    } else {
+      // If a parallel block models 1 or more threads, it may not write to stack variables, as this
+      // could potentially cause data races between threads.
+      if (writeSets(parallelBlock).nonEmpty) {
+        val msg = "Parallel block modelling multiple threads cannot write to shared stack variables"
+        parallelBlock.getOrigin.report("error", msg)
+        Abort("")
       }
     }
   }
@@ -61,7 +63,7 @@ class LocalVariableChecker(arg: ProgramUnit) extends RecursiveVisitor[Unit](arg)
     val badWrites = readOnlyNames.intersect(writeSet)
 
     if (badWrites.nonEmpty) {
-      invariant.getOrigin.report("error", "The following vars appear in the invariant but are also writting to in the invariant body: %s", badWrites.mkString(", "))
+      invariant.getOrigin.report("error", "The following vars appear in the invariant but are also written to in the invariant body: %s", badWrites.mkString(", "))
       Abort("Invariant writes to local variables that appear in invariant")
     }
   }
