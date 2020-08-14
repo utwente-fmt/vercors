a0d66a31fccbb4149f1bfd50fbd94148bf4a7467
Bob Rubbens <bobrubbens@gmail.com>
2020-08-14 16:57:50 +0200

    Typo!

--- examples/abrupt/KeYAbruptTerminationChallenge.java
+++ examples/abrupt/KeYAbruptTerminationChallenge.java
@@ -12,7 +12,7 @@ class AbruptTermination {
 
     //@ context_everywhere Perm(ia, read) ** ia != null;
     //@ context_everywhere (\forall* int i; 0 <= i && i < ia.length; Perm(ia[i], write));
-    // ensures ia.length == \old(ia.length); // Extra contract needed for VerCors
+    //@ ensures ia.length == \old(ia.length); // Extra contract needed for VerCors
     // ensure i is the first position with negative value
     /*@ ensures (\forall int i; 0 <= i && i < ia.length;
                     (\old(ia[i]) < 0 && (\forall int j; 0 <= j && j < i; \old(ia[j]) >= 0))
