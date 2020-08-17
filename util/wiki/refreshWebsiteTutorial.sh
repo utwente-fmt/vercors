93cb6a2714c7e87f94670d8dbb15af68ee1a8f35
Bob Rubbens <bobrubbens@gmail.com>
2020-08-17 17:34:19 +0200

    Progress on refresh script

--- util/wiki/refreshWebsiteTutorial.sh
+++ util/wiki/refreshWebsiteTutorial.sh
@@ -11,19 +11,26 @@ echo "Running generate_wiki_pdf.py"
 python3 generate_wiki_pdf.py \
         -w vercors-web/views/site/_wiki_content.php \
         -m vercors-web/views/site/_wiki_menu.php
-cd vercors-web.git
-git commit -am "Update website tutorial from github wiki"
-git push
+cd vercors-web
+#git commit -am "Update website tutorial from github wiki"
+#git push
 cd ..
-rm -rf vercors-web
 
 echo "vercors-web updated, don't forget to also deploy it!"
 
 # get certificate manually
-# echo | openssl s_client -connect 130.89.1.50:443 | openssl x509 -out ./curlftpcert.pem
+echo | openssl s_client -connect 130.89.1.50:443 | openssl x509 -out ./curlftpcert.pem
 
 # mount
-# curlftpfs -o ssl,cacert=./curlftpcert.pem,no_verify_peer,user=u869582 webservice.utwente.nl ./ftpmnt/
+mkdir ftpmnt
+curlftpfs -o ssl,cacert=./curlftpcert.pem,no_verify_peer,user=u869582 webservice.utwente.nl ./ftpmnt/
 
-# umount ftpmnt
-# rm curlftpcert.pem
+changedFiles=$(cd vercors-web; git diff --name-only)
+
+printf "$changedFiles\n"
+
+#umount ftpmnt
+#rm curlftpcert.pem
+#rm -r ftpmnt
+#rm vercors-web -rf
+
