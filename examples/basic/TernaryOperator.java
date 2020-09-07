c21e01b4e99b41e747a7a53459a1d92fda5fec22
Dré van Oorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2020-09-07 17:27:32 +0200

    Fix new Ternary Operator test

--- examples/basic/TernaryOperator.java
+++ examples/basic/TernaryOperator.java
@@ -1,3 +1,8 @@
+// -*- tab-width:4 ; indent-tabs-mode:nil -*-
+//:: case TernaryOperator
+//:: tools silicon
+//:: verdict Pass
+
 class Test {
     void usage(boolean cond) {
         int result = cond ? expectEqual(cond, true) : expectEqual(cond, false);
