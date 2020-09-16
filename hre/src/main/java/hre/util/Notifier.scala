96b57b34e969cae4df47adb256f9d8a673666ecd
Bob Rubbens <bobrubbens@gmail.com>
2020-09-16 12:51:36 +0200

    First draft of windows support for notifications

--- hre/src/main/java/hre/util/Notifier.scala
+++ hre/src/main/java/hre/util/Notifier.scala
@@ -1,6 +1,6 @@
 package hre.util
 
-import java.io.File
+import java.io.{ByteArrayInputStream, File}
 
 import sys.process._
 import hre.lang.System.Warning
@@ -46,11 +46,47 @@ object Notifier {
     }
   }
 
+  val powershellNotificationScript =
+    """
+      |$app = '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe'
+      |[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime]
+      |
+      |$Template = [Windows.UI.Notifications.ToastTemplateType]::ToastImageAndText01
+      |
+      |#Gets the Template XML so we can manipulate the values
+      |[xml]$ToastTemplate = ([Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent($Template).GetXml())
+      |
+      |[xml]$ToastTemplate = @"
+      |<toast launch="app-defined-string">
+      |  <visual>
+      |    <binding template="ToastGeneric">
+      |      <text>%s</text>
+      |      <text>%s</text>
+      |    </binding>
+      |  </visual>
+      |  <actions>
+      |    <action activationType="background" content="Remind me later" arguments="later"/>
+      |  </actions>
+      |</toast>
+      |"@
+      |
+      |$ToastXml = New-Object -TypeName Windows.Data.Xml.Dom.XmlDocument
+      |$ToastXml.LoadXml($ToastTemplate.OuterXml)
+      |
+      |$notify = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($app)
+      |
+      |$notify.Show($ToastXml)
+      |""".stripMargin
+
   def notifyWindows10(title: String, message: String): Boolean = {
     // https://gist.github.com/altrive/72594b8427b2fff16431
     // This one seems better: https://gist.github.com/Windos/9aa6a684ac583e0d38a8fa68196bc2dc
-    Warning("Notifying on windows 10 not yet supported")
-    false
+
+    val script = powershellNotificationScript.format(title, message)
+    val is = new ByteArrayInputStream(script.getBytes("UTF-8"))
+    val cmd = Seq("powershell", "-Command", "-")
+    val p2 = cmd #< is
+    p2.!(ProcessLogger(Warning("%s", _), Warning("%s", _))) == 0
   }
 
   def commandExists(cmd: String): Boolean = {
