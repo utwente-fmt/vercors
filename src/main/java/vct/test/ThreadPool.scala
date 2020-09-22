d719a219892b9720d1756fd8c2628904180e543d
Pieter Bos <p.h.bos@student.utwente.nl>
2020-09-22 13:59:25 +0200

    make thread pool a bit nicer

--- src/main/java/vct/test/ThreadPool.scala
+++ src/main/java/vct/test/ThreadPool.scala
@@ -7,13 +7,20 @@ import hre.lang.System.Output
 import scala.collection.JavaConverters._
 import scala.collection.mutable
 
+/**
+  * Custom thread pool implementation that returns a stream with detailed progress information
+  * @param threadCount The number of threads to spin up once started
+  * @param tasks The tasks to run
+  * @tparam T Task type, should be Callable[?]
+  * @tparam S Only there to capture the result type of Callable
+  */
 case class ThreadPool[T <: Callable[S], S](threadCount: Int, tasks: Seq[T]) {
   val taskQueue: mutable.ArrayBuffer[T] = mutable.ArrayBuffer()
   val taskResult: LinkedBlockingDeque[(T, S, Seq[T], Int)] = new LinkedBlockingDeque()
   // ordered buffer, so the oldest work appears in front
   val runningWork: mutable.ArrayBuffer[T] = mutable.ArrayBuffer()
 
-  def fetchWork(): Option[T] = this.synchronized {
+  private def fetchWork(): Option[T] = this.synchronized {
     if(taskQueue.isEmpty) {
       None
     } else {
@@ -23,13 +30,14 @@ case class ThreadPool[T <: Callable[S], S](threadCount: Int, tasks: Seq[T]) {
     }
   }
 
-  def replaceWork(work: T, result: S): Option[T] = this.synchronized {
+  private def replaceWork(work: T, result: S): Option[T] = this.synchronized {
     runningWork -= work
     val newWork = fetchWork()
     taskResult.push((work, result, runningWork.clone(), taskQueue.size))
     newWork
   }
 
+  /** Start worker threads in the background */
   def start(): Unit = {
     taskQueue ++= tasks
     val threads = (0 until threadCount).map(_ => new Thread(() => {
@@ -41,9 +49,8 @@ case class ThreadPool[T <: Callable[S], S](threadCount: Int, tasks: Seq[T]) {
     threads.foreach(_.start())
   }
 
+  /** Present the results of the tasks as a stream out of order: returns task, task result, currently running tasks and
+    * remaining number of queued tasks */
   def results(): Stream[(T, S, Seq[T], Int)] =
-    tasks.toStream.map(_ => {
-      val resultToPut = taskResult.take()
-      resultToPut
-    })
+    tasks.indices.toStream.map(_ => taskResult.take())
 }
