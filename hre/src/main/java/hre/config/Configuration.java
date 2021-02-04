344ec60bdb5eb95271ef9649c81d465928a7d21d
Armborst <l.armborst@utwente.nl>
2021-02-04 14:29:28 +0100

    improved Windows support: use proper TEMP environment variable

--- hre/src/main/java/hre/config/Configuration.java
+++ hre/src/main/java/hre/config/Configuration.java
@@ -276,6 +276,7 @@ public class Configuration {
         env.addArg("-Xss128M");
         env.addArg("-cp", System.getProperty("java.class.path"));
         env.addArg("vct.main.Main");
+        env.setEnvironmentVar("TEMP", System.getenv("TEMP"));
         return env;
     }
 
