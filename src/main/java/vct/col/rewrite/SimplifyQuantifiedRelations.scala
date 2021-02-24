3be03e4aaa11a60812f6a45efcd96aa3f8176c07
Pieter Bos <p.h.bos@student.utwente.nl>
2021-02-24 16:54:26 +0100

    typo

--- src/main/java/vct/col/rewrite/SimplifyQuantifiedRelations.scala
+++ src/main/java/vct/col/rewrite/SimplifyQuantifiedRelations.scala
@@ -290,7 +290,7 @@ class SimplifyQuantifiedRelations(source: ProgramUnit) extends AbstractRewriter(
           case None => super.visit(expr); return
           case Some(main) => main
         }
-        result = create expression(Implies, (indepSelect ++ bounds.selectNonEmpty).reduce(and), claim))
+        result = create expression(Implies, (indepSelect ++ bounds.selectNonEmpty).reduce(and), claim)
       case _ =>
         super.visit(expr)
     }
