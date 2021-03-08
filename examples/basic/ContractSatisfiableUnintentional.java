c7cfc930a32e031077499567fd6ff65b4fbbf15d
Bob Rubbens <bobrubbens@gmail.com>
2021-03-08 17:06:26 +0100

    Split up contract (un)satisfiable tests.

---
+++ examples/basic/ContractSatisfiableUnintentional.java
@@ -0,0 +1,11 @@
+//:: cases ContractSatisfiableUnintentionalJava
+//:: tools silicon
+//:: verdict Fail
+
+class MyClass {
+  // User makes a mistake here, this should be detected.
+  //@ requires 3 == 4;
+  void bar() {
+    //@ assert 5 == 6;
+  }
+}
