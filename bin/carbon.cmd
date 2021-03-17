ae91c30770cf98c6b7fd0f557f291380ae62f9eb
Armborst <l.armborst@utwente.nl>
2021-03-17 15:00:54 +0100

    Fixed missing/wrong file paths in Windows scripts for silicon and carbon

--- bin/carbon.cmd
+++ bin/carbon.cmd
@@ -4,7 +4,7 @@ setlocal
 rem %~dp0 is expanded pathname of the current script under NT, i.e. the "bin" directory
 set BIN=%~dp0
 
-set BOOGIE_EXE="%BIN%\..\src\main\universal\res\deps\boogie\1.0.0.0-carbon\Windows\Boogie.exe"
+set BOOGIE_EXE=C:\test with space\Windows\Boogie.exe
 set Z3="%BIN%\..\src\main\universal\res\deps\z3\4.8.6\Windows NT\intel\bin\z3.exe"
 
 call "%BIN%\run-class.cmd" viper.carbon.Carbon --z3Exe %Z3% %*
