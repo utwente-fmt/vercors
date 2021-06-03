4c53d98815e90e67a8bff186a650adcead4cd144
ivan@edmi.nl <060599IJh>
2021-06-03 10:25:06 +0200

    rewriter test for FilterSpecIgnore

---
+++ src/test/scala/vct/helper/AstComparer.scala
@@ -0,0 +1,119 @@
+package vct.helper
+
+import vct.col.ast.{Constant, Declaration, Node, Program, Ref}
+
+import scala.collection.mutable
+
+
+/**
+ * This object is to compare two programs of the ast.
+ */
+object AstComparer {
+
+
+  /**
+   * Throws an AstUnequalException when the Programs are not equal
+   */
+  def astProgramEquals(left: Program, right: Program): Unit ={
+    val succession: mutable.Map[Declaration, Declaration] = mutable.Map[Declaration, Declaration]()
+    val leftGlobalDeclarations = left.decls
+    val rightGlobalDeclarations = right.decls
+    if (leftGlobalDeclarations.size != rightGlobalDeclarations.size) {
+      throw AstUnequalException("Mismatched globalDeclaration length",left,right)
+    }
+    succession ++= leftGlobalDeclarations.zip(rightGlobalDeclarations).toMap
+
+    for((leftDeclaration, rightDeclaration) <- leftGlobalDeclarations.zip(rightGlobalDeclarations)) {
+      astNodeEquals(leftDeclaration, rightDeclaration, succession)
+    }
+  }
+
+  /**
+   * Throws an AstUnequalException when the Nodes are not equal
+   */
+  def astNodeEquals(left: Node, right: Node, succession: mutable.Map[Declaration, Declaration]): Unit = {
+    val leftDecls = left.subnodes.collect { case decl: Declaration => decl }
+    val rightDecls = right.subnodes.collect { case decl: Declaration => decl }
+    if (leftDecls.size != rightDecls.size) {
+      throw AstUnequalException("Mismatched scope length",left,right)
+    }
+    succession ++= leftDecls.zip(rightDecls).toMap
+
+    if (left.getClass != right.getClass) {
+      val leftClass = left.getClass
+      val rightClass = right.getClass
+      throw AstUnequalException(s"The kind $leftClass is not the same as $rightClass",left,right)
+    }
+
+    if (left.subnodes.size != right.subnodes.size) {
+      throw AstUnequalException("Unequal number of subnodes",left,right)
+    }
+
+    (left, right) match {
+      case (left: Declaration, right: Declaration) =>
+        astDeclarationEquals(left, right, succession)
+      case (left: scala.Product, right: scala.Product) =>
+        astProductEquals(left, right, succession)
+      case(left: Constant[_], right: Constant[_]) =>
+        astConstantEquals(left, right)
+      case (_, _) =>
+        throw AstUnequalException("Incomparable nodes",left,right)
+    }
+    for ((left, right) <- left.subnodes.zip(right.subnodes)) {
+      astNodeEquals(left, right, succession)
+    }
+  }
+
+  /**
+   * Throws an AstUnequalException when the values of the products are not equal. It does not check the subnodes
+   */
+  def astProductEquals(leftProduct: scala.Product, rightProduct: scala.Product, succession: mutable.Map[Declaration, Declaration]): Unit = {
+    for ((leftValue, rightValue) <- leftProduct.productIterator.zip(rightProduct.productIterator)) {
+      astValueEquals(leftValue,rightValue,succession)
+    }
+  }
+
+  /**
+   * Throws an AstUnequalException when the values are not equal. It does compare nodes.
+   */
+  def astValueEquals(left: Any, right: Any, succession: mutable.Map[Declaration, Declaration]): Unit ={
+    (left,right) match{
+      case (left: Ref[Declaration], right: Ref[Declaration]) =>
+        astDeclarationEquals(left.decl, right.decl, succession)
+      case (_: Node, _: Node) =>
+        //subnodes are compared in astNodeEquals
+      case (leftList: List[_], rightList: List[_]) =>
+        for ((leftListItem, rightListItem) <- leftList.zip(rightList)) {
+          astValueEquals(leftListItem,rightListItem,succession)
+        }
+      case (left, right) =>
+        if (left != right)
+          throw AstUnequalException("Unequal values",left,right)
+    }
+  }
+
+  /**
+   * Throws an AstUnequalException when the declarations do not references the same declaration. It does not check the subnodes or values.
+   */
+  def astDeclarationEquals(left: Declaration, right: Declaration, succession: mutable.Map[Declaration, Declaration]): Unit = {
+    //TODO: Does not check all values of the declaration. The subnodes are checked in astNodeEquals.
+    succession.get(left) match {
+      case Some(decl) if decl == right =>
+      case Some(other) =>
+        throw AstUnequalException("Expected declaration of the right node but got another node",left,right,other)
+      case None =>
+        throw AstUnequalException("Could not find declaration for the left node",left,right)
+    }
+  }
+
+  /*
+   * Throws an AstUnequalException when values of the constants are not equal.
+   */
+  def astConstantEquals(left: Constant[_], right: Constant[_]): Unit = {
+    if(left.value != right.value){
+      throw AstUnequalException("Value of constants are not the same",left,right)
+    }
+  }
+
+
+}
--- src/test/scala/vct/newrewrite/FilterSpecIgnoreSpec.scala
+++ src/test/scala/vct/newrewrite/FilterSpecIgnoreSpec.scala
@@ -2,164 +2,114 @@ package vct.newrewrite
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import vct.col.ast.Constant._
 import vct.col.ast._
 import vct.col.newrewrite.FilterSpecIgnore
-
-import java.nio.file.Paths
-import scala.Console
+import vct.helper.{AstComparer, RewriteTestHelper, SimpleProgramGenerator}
 
 
 class FilterSpecIgnoreSpec extends AnyFlatSpec with Matchers {
 
-  it should "not change anything given tree without filterSpecIgnore" in {
-    var treeInput: Program = null
+  //nothing should change
+  //should remove something
+  //throw error with unbalanced tree 2x
+
+  val rewriter = FilterSpecIgnore()
+
+  it should "not change anything given tree without filterSpecIgnore expect for DirectRef to LazyRef" in {
+    var programInput: Program = null
     var programExpectedOutput: Program = null
 
     {
-      implicit val origin: InputOrigin = FileOrigin(Paths.get(""), 1, 1, 1, 1)
-      val contract1 = ApplicableContract(BooleanValue(value = true),BooleanValue(value = true),BooleanValue(value = true),Seq(),Seq(),Seq())
-      val variable1 = new Variable(TInt())
-      val body1 = Block(Seq(
-        LocalDecl(variable1),
-        SpecIgnoreStart(),
-        Eval(Plus(Local(new DirectRef[Variable](variable1)), Local(new DirectRef[Variable](variable1)))),
-        SpecIgnoreEnd(),
-        Return(Local(new DirectRef[Variable](variable1)))
+      implicit val origin: InputOrigin = SimpleProgramGenerator.GenerateSimpleInputOrigin()
+      val variable = new Variable(TInt())
+      val body = Block(Seq(
+        LocalDecl(variable),
+        Eval(Plus(Local(new DirectRef[Variable](variable)), Local(new DirectRef[Variable](variable)))),
+        Return(Local(new DirectRef[Variable](variable)))
       ))
-      val blame1 = origin
-      val method1 = new InstanceMethod(TVoid(), Seq(), Seq(), Option(body1), contract1)(blame1)
-      val classNode1 = new Class(Seq(method1))
-      treeInput = Program(Seq(classNode1))
+      programInput = SimpleProgramGenerator.GenerateProgramWithSingleClassAndSingleMethod(body)
     }
 
     {
-      implicit val origin2: InputOrigin = FileOrigin(Paths.get(""), 1, 1, 1, 1)
-      val variable2 = new Variable(TInt())
-      val contract2 = ApplicableContract(BooleanValue(value = true),BooleanValue(value = true),BooleanValue(value = true),Seq(),Seq(),Seq())
-      val body2 = Block(Seq(
-        LocalDecl(variable2),
-        Return(Local(new DirectRef[Variable](variable2)))
+      implicit val origin: InputOrigin = SimpleProgramGenerator.GenerateSimpleInputOrigin()
+      val variable = new Variable(TInt())
+      val body = Block(Seq(
+        LocalDecl(variable),
+        Eval(Plus(Local(new LazyRef[Variable](variable)), Local(new LazyRef[Variable](variable)))),
+        Return(Local(new LazyRef[Variable](variable)))
       ))
-      val blame2 = origin2
-      val method2 = new InstanceMethod(TVoid(), Seq(), Seq(), Option(body2), contract2)(blame2)
-      val classNode2 = new Class(Seq(method2))
-      programExpectedOutput = Program(Seq(classNode2))
-
+      programExpectedOutput = SimpleProgramGenerator.GenerateProgramWithSingleClassAndSingleMethod(body)
     }
-
-    val rewriter = FilterSpecIgnore()
-    val programActualOutput = rewriter.rewrite(treeInput)
-
-    val result = programEquals(programActualOutput,programExpectedOutput)
-    result match{
-      case Right(_) =>
-        assert(true)
-      case Left(errorMessage) =>
-        Console.println(errorMessage)
-        assert(false)
-    }
-    assert(result.isRight)
+    RewriteTestHelper.Test(rewriter, programInput, programExpectedOutput)
   }
 
-  //TODO: replace to string to a col printer?
-  //TODO: replace all left with error throwing. It is easier with debugger.
-  def programEquals(left: Program, right: Program): Either[String, Unit] ={
+  it should "remove nodes within filterSpecIgnore" in {
+    var programInput: Program = null
+    var programExpectedOutput: Program = null
 
-    throw AstUnequalException("test",left,right)
-
-    val succession: Map[Declaration, Declaration] = Map()
-    val leftGlobalDeclarations = left.decls
-    val rightGlobalDeclarations = right.decls
-    if (leftGlobalDeclarations.size != rightGlobalDeclarations.size) {
-      return Left(s"Mismatched globalDeclaration length in $left and $right")
+    {
+      implicit val origin: InputOrigin = SimpleProgramGenerator.GenerateSimpleInputOrigin()
+      val variable = new Variable(TInt())
+      val body = Block(Seq(
+        LocalDecl(variable),
+        SpecIgnoreStart(),
+        Eval(Plus(Local(new DirectRef[Variable](variable)), Local(new DirectRef[Variable](variable)))),
+        SpecIgnoreEnd(),
+        Return(Local(new DirectRef[Variable](variable)))
+      ))
+      programInput = SimpleProgramGenerator.GenerateProgramWithSingleClassAndSingleMethod(body)
     }
-    val moreSuccession = succession ++ leftGlobalDeclarations.zip(rightGlobalDeclarations).toMap
 
-    for((leftDeclaration, rightDeclaration) <- leftGlobalDeclarations.zip(rightGlobalDeclarations)) {
-      val result = astEquals(leftDeclaration, rightDeclaration, moreSuccession)
-      if (result.isLeft) return result
+    {
+      implicit val origin: InputOrigin = SimpleProgramGenerator.GenerateSimpleInputOrigin()
+      val variable = new Variable(TInt())
+      val body = Block(Seq(
+        LocalDecl(variable),
+        Return(Local(new LazyRef[Variable](variable)))
+      ))
+      programExpectedOutput = SimpleProgramGenerator.GenerateProgramWithSingleClassAndSingleMethod(body)
     }
-    Right()
+    RewriteTestHelper.Test(rewriter, programInput, programExpectedOutput)
   }
 
-  def astEquals(left: Node, right: Node, succession: Map[Declaration, Declaration]): Either[String, Unit] = {
-    val leftDecls = left.subnodes.collect { case decl: Declaration => decl }
-    val rightDecls = right.subnodes.collect { case decl: Declaration => decl }
-    if (leftDecls.size != rightDecls.size) {
-      return Left(s"Mismatched scope length in $left and $right")
-    }
-    val moreSuccession = succession ++ leftDecls.zip(rightDecls).toMap
+  it should "throw error with two many SpecIgnoreStart" in {
+    var programInput: Program = null
+    var programExpectedOutput: Program = null
 
-    if (left.getClass != right.getClass) {
-      val leftClass = left.getClass
-      val rightClass = right.getClass
-      return Left(s"The kind $leftClass is not the same as $rightClass. From objects $left and $right respectively")
-    }
-
-    if (left.subnodes.size != right.subnodes.size) {
-      return Left(s"The tree $left does not have the same number of subnodes as $right")
+    {
+      implicit val origin: InputOrigin = SimpleProgramGenerator.GenerateSimpleInputOrigin()
+      val variable = new Variable(TInt())
+      val body = Block(Seq(
+        LocalDecl(variable),
+        SpecIgnoreStart(),
+        Eval(Plus(Local(new DirectRef[Variable](variable)), Local(new DirectRef[Variable](variable)))),
+        Return(Local(new DirectRef[Variable](variable)))
+      ))
+      programInput = SimpleProgramGenerator.GenerateProgramWithSingleClassAndSingleMethod(body)
     }
 
-    (left, right) match {
-      case (left: Declaration, right: Declaration) =>
-        astDeclarationEquals(left, right, moreSuccession)
-      case (left: scala.Product, right: scala.Product) =>
-        astProductEquals(left,right,succession)
-      case (_, _) =>
-        Left(s"Incomparable: $left and $right")
-    }
+    rewriter.rewrite(programInput)
+    throw new NotImplementedError()
   }
 
-  def astProductEquals(left: scala.Product, right: scala.Product, succession: Map[Declaration, Declaration]): Either[String, Unit] = {
-    for ((leftValue, rightValue) <- left.productIterator.zip(right.productIterator)) {
-      val result = astValueEquals(leftValue, rightValue, succession)
-      if (result.isLeft) return result
-    }
-    Right()
-  }
+  it should "throw error with two many SpecIgnoreEnd" in {
+    var programInput: Program = null
+    var programExpectedOutput: Program = null
 
-  def astValueEquals(left: Any, right: Any, succession: Map[Declaration, Declaration]): Either[String, Unit] = (left, right) match {
-    case (left: Ref[Declaration], right: Ref[Declaration]) =>
-      astDeclarationEquals(left.decl, right.decl, succession)
-    case (left: Node, right: Node) =>
-      astEquals(left, right, succession)
-    case (left, right) =>
-      if (left == right) Right(())
-      else Left(s"Inequal values: $left and $right")
-  }
-
-  def astDeclarationEquals(left: Declaration, right: Declaration, succession: Map[Declaration, Declaration]): Either[String, Unit] = {
-    succession.get(left) match {
-      case Some(decl) if decl == right =>
-        //TODO: does not check all values of the declaration
-        for ((left, right) <- left.subnodes.zip(right.subnodes)) {
-          val result = astEquals(left, right, succession)
-          if (result.isLeft) return result
-        }
-        Right(())
-      case Some(other) =>
-        Left(s"Comparing $right we expected a declaration matching $left, " +
-          s"but the matching declaration of that is $other")
-      case None =>
-        Left(s"Comparing $right we expected a declaration matching $left, " +
-          s"but it has no matching declaration.")
-    }
-  }
-
-
-  case class AstUnequalException(message: String,left: Any, right: Any) extends Exception(this.toString()) {
-
-    override def toString: String = {
-      var completeMessage = message
-      completeMessage += "\n";
-      completeMessage += left;
-      completeMessage += "\n";
-      completeMessage += right;
-      return completeMessage
+    {
+      implicit val origin: InputOrigin = SimpleProgramGenerator.GenerateSimpleInputOrigin()
+      val variable = new Variable(TInt())
+      val body = Block(Seq(
+        LocalDecl(variable),
+        Eval(Plus(Local(new DirectRef[Variable](variable)), Local(new DirectRef[Variable](variable)))),
+        SpecIgnoreEnd(),
+        Return(Local(new DirectRef[Variable](variable)))
+      ))
+      programInput = SimpleProgramGenerator.GenerateProgramWithSingleClassAndSingleMethod(body)
     }
 
+    rewriter.rewrite(programInput)
+    throw new NotImplementedError()
   }
-
 
 }
