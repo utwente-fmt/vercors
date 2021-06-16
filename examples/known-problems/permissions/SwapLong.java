ac49773c103a4d45e925c7dafb704285743fb879
ivan@edmi.nl <060599IJh>
2021-06-16 17:12:47 +0200

    removed skip-travis and moved all problem-fail files

---
+++ examples/known-problems/permissions/SwapLong.java
@@ -0,0 +1,50 @@
+// -*- tab-width:2 ; indent-tabs-mode:nil -*-
+//:: cases SwapLong
+//:: suite problem-fail
+//:: tools silicon
+//:: verdict Pass SwapLong.n SwapLong.twice
+//:: verdict Fail SwapLong.wrong
+
+// Issue with Long things in VerCors
+
+public class SwapLong {
+
+  long F;
+  long G;
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(G) && G == \old(F);
+  @*/
+  void n()
+  {
+    long tmp;
+    tmp = F;
+    F = G;
+    G = tmp;
+  }
+
+
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(F) && G == \old(G);
+  @*/
+  void twice()
+  {
+    n();
+    n();
+  }
+  
+  /*@ 
+    requires Perm(F,1) ** Perm(G,1);
+    ensures Perm(F,1) ** Perm(G,1);
+    ensures F == \old(F) && G == \old(G);
+  @*/
+  void wrong()
+  {
+    n();
+  }
+
+}
+
