5146de3d4010831c26c29d315b0337e1360b51b1
Bob Rubbens <bobrubbens@gmail.com>
2021-07-08 15:53:59 +0200

    Fix sequential tests

---
+++ examples/sequential/BoogieExampleFail.java
@@ -0,0 +1,39 @@
+//:: cases BoogieExampleFail
+//:: tools silicon
+//:: verdict Fail
+
+public class BoogieExample {
+
+  /*@
+    ensures \result <= 91;     // Do you believe this one is true?
+  @*/
+  public static int G(int n) {
+    int r;
+    if (100 < n) {
+      r = n - 10;
+      return r;
+    } else {
+      r = F(n + 11);
+      r = F(r);
+      return r;
+    }
+  }
+
+  /*@
+    ensures 100 < n ==> \result == n - 10;  // This postcondition is easy to check by hand
+    ensures n <= 100 ==> \result == 91;     // Do you believe this one is true?
+  @*/
+  public static int F(int n) {
+    int r;
+    if (100 < n) {
+      r = n - 10;
+    } else {
+      r = F(n + 11);
+      r = F(r);
+    }
+    return r;
+  }
+
+}
+
+
