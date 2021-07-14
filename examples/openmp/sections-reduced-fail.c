be901207d92f86df9f8ada6658216d3ef5a62de6
Bob Rubbens <bobrubbens@gmail.com>
2021-07-14 14:19:53 +0200

    Mark sections-reduced-fail.c as Pass

--- examples/openmp/sections-reduced-fail.c
+++ examples/openmp/sections-reduced-fail.c
@@ -1,7 +1,7 @@
 // -*- tab-width:2 ; indent-tabs-mode:nil -*-
 //:: case OpenMPSectionReducedFail1
 //:: tools silicon
-//:: verdict Fail
+//:: verdict Pass
 //:: option --stop-before-backend
 //:: suite problem-partial
 
