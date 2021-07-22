1a35a2bc8ef165972f9b45555a64808a1d4f64f5
Ivan Hop <ivan@edmi.nl>
2021-07-22 14:11:43 +0200

    removed unused code from src/rewrite

--- src/main/java/vct/col/rewrite/Flatten.java
+++ src/main/java/vct/col/rewrite/Flatten.java
@@ -225,14 +225,13 @@ public class Flatten extends AbstractRewriter {
 
   @Override
   public void visit(ParallelBlock pb){
-    ParallelBlock res=create.parallel_block(
+    result= create.parallel_block(
             pb.label(),
             rewrite(pb.contract()),
             copy_pure.rewrite(pb.itersJava()),
             rewrite(pb.block()),
             rewrite(pb.deps())
     );
-    result=res;
   }
 
   @Override
@@ -436,7 +435,6 @@ public class Flatten extends AbstractRewriter {
       boolean derefItem = false;
 
       if(arg.isPrimitive(PrimitiveSort.Cell)) {
-        arg = (Type) arg.firstarg();
         derefItem = true;
       }
 
