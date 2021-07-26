bf59ed3e9345537a4b1d69ecbc5f2ccbc7b64972
Bob Rubbens <bobrubbens@gmail.com>
2021-07-26 13:51:19 +0200

    Further implement test suite and fix veymont tests

--- util/veymontTesting/veymont-2stage-tests.sh
+++ util/veymontTesting/veymont-2stage-tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# This is a temporary veymont testing script. It finds all cases in examples/veymont-global-programs, runs them through
-# veymont, and then runs the output through vercors. It ignores all other features of the vercors testing framework
-# (e.g. suites, options). If this script gets any more features it should, at least, be re-implemented in scala,
-# but ideally it would be integrated in the test suite.
+# This is a temporary veymont testing script.
+#
+# It finds all cases in examples/veymont-global-programs, runs them through veymont, and runs the output
+# through vercors.
+#
+# It ignores all other features of the vercors testing framework (e.g. suites, options). If this script gets any more
+# features it should, at least, be re-implemented in scala, but ideally it would be integrated in the test suite.
 
 vercorsPath=$(which vct-dev || which vct || which vercors)
 if [ -z $vercorsPath ]; then
@@ -12,25 +15,22 @@ if [ -z $vercorsPath ]; then
 fi
 echo "VerCors path: $vercorsPath"
 
-# Move to the directory of the shell script
+# Cd to the directory of the shell script
 cd "$(dirname "$0")"
-# Move to veymont global tests
+
 cd ../../examples/veymont-global-programs
 
-# Collect all cases. For now we only collect the first 4 cases of each file, as there are no tests with more than 2 cases anyway
-cases1=$(grep -r cases | cut -d " " -f 3)
-#                |           |      - Print the third element after splitting
-#                |           - Split at space
-#                - search for "cases"
-cases2=$(grep -r cases | cut -d " " -f 4)
-cases3=$(grep -r cases | cut -d " " -f 5)
-cases4=$(grep -r cases | cut -d " " -f 6)
-# The awk invocation drops all empty lines from stdin and prints the leftover lines
-# Also, use printf because echo ignores \n newlines
-allCases=$(printf "$cases1\n$cases2\n$cases3\n$cases4\n" | awk NF | sort -u)
+# Collect all cases. The cut part of the invocation selects the third element after splitting on spaces.
+allCases=$(grep -r cases | cut -d " " -f 3 | sort -u)
 
-echo "##### Detected cases: #####"
+echo "##### Detected cases for 2-stage tests: #####"
 echo "$allCases"
+
+failingReport="##### Failing 2-stage cases #####"
+# This is the final return code to be returned
+# In unix-style, 0 is success, anything else is failure
+# So if at some point it is set to something non-zero, it means something went wrong
+totalReturnCode=0
 
 for kees in $allCases
 do
@@ -45,7 +45,9 @@ do
   echo "$output"
   if [[ $output != *"Pass"* ]]; then
     echo "VeyMont did not succeed in analyzing case $kees, files: $files"
-    exit 1
+    failingReport="$failingReport\n- $kees"
+    totalReturnCode=1
+    continue
   fi
 
   vercorsCmd="$vercorsPath --silicon ${kees}_output.pvl"
@@ -54,6 +56,10 @@ do
   echo "$output"
   if [[ $output != *"Pass"* ]]; then
     echo "VerCors did not succeed in analyzing the VeyMont output for case $kees"
-    exit 1
+    failingReport="$failingReport\n- $kees"
+    totalReturnCode=1
   fi
 done
+
+printf "$failingReport\n"
+exit $totalReturnCode
