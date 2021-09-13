aecf06f7fde772700db72c292746aff0b2a4c103
Bob Rubbens <bobrubbens@gmail.com>
2021-09-13 17:48:29 +0200

    Use custom origin for ContinueToBreak

--- src/main/java/vct/col/newrewrite/ContinueToBreak.scala
+++ src/main/java/vct/col/newrewrite/ContinueToBreak.scala
@@ -6,6 +6,11 @@ import vct.col.ast._
 
 import scala.collection.mutable
 
+case class ContinueToBreakOrigin(loopLabel: String, inner: Origin) extends Origin {
+  override def preferredName: String = "continue" + loopLabel.capitalize
+  override def messageInContext(message: String): String = inner.messageInContext(message)
+}
+
 case class ContinueToBreak() extends Rewriter {
   val loopLabelToInnerLabel = new mutable.HashMap[LabelDecl, LabelDecl]()
 
@@ -14,7 +19,7 @@ case class ContinueToBreak() extends Rewriter {
       val rewrittenBody = rewriteDefault(loop.body)
 
       // If the loop label appears in the mapping, it means it contains some continue that wants to break
-      // from the inner label. We use create a block wrapping the body, labeled with the inner label.
+      // from the inner label. We create a block wrapping the body, labeled with the inner label.
       val possiblyWrappedBody = loopLabelToInnerLabel.get(labelDecl) match {
         case Some(innerLabelDecl) =>
           implicit val o = loop.o
@@ -22,14 +27,14 @@ case class ContinueToBreak() extends Rewriter {
         case None => rewrittenBody
       }
 
-      block.rewrite(Seq(rewriteDefault(l), loop.rewrite(body = possiblyWrappedBody)))
+      block.rewrite(Seq(l.rewrite(), loop.rewrite(body = possiblyWrappedBody)))
 
     case c@Continue(Some(Ref(labelDecl: LabelDecl))) =>
       // Reuse an already created inner label or create one
       val innerLabelDecl = loopLabelToInnerLabel.get(labelDecl) match {
         case Some(l) => l
         case None =>
-          val innerLabelDecl = new LabelDecl()(SourceNameOrigin("continue", labelDecl.o))
+          val innerLabelDecl = new LabelDecl()(ContinueToBreakOrigin(labelDecl.o.preferredName, labelDecl.o))
           loopLabelToInnerLabel.put(labelDecl, innerLabelDecl)
           innerLabelDecl
       }
