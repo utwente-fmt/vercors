60c4751bc5695ef9d8973d1741a6b897adea75fe
Bob Rubbens <bobrubbens@gmail.com>
2021-09-13 17:27:11 +0200

    Add basic testing for SpecifyImplicitLabels

---
+++ src/test/scala/vct/newrewrite/SpecifyImplicitLabelsSpec.scala
@@ -0,0 +1,182 @@
+package vct.newrewrite
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import vct.col.ast._
+import vct.col.newrewrite.SpecifyImplicitLabels
+import vct.helper.{RewriteTestHelper, SimpleProgramGenerator}
+
+class SpecifyImplicitLabelsSpec extends AnyFlatSpec with Matchers {
+
+  implicit val o = DiagnosticOrigin
+
+  it should "add a label to a switch without a label" in {
+    val before = {
+      Switch(
+        Constant.IntegerValue(5),
+        Block(Seq(
+          Case(Constant.IntegerValue(0)),
+          Break(None),
+          Case(Constant.IntegerValue(5)),
+          Break(None),
+        ))
+      )
+    }
+
+    val after = {
+      val switchLabel = new LabelDecl()
+      Block(Seq(
+        Label(switchLabel),
+        Switch(
+          Constant.IntegerValue(5),
+          Block(Seq(
+            Case(Constant.IntegerValue(0)),
+            Break(Some(switchLabel.ref)),
+            Case(Constant.IntegerValue(5)),
+            Break(Some(switchLabel.ref)),
+          ))
+        )
+      ))
+    }
+
+    val rewriter = SpecifyImplicitLabels()
+    RewriteTestHelper.test(
+      rewriter,
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(before),
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(after)
+    )
+    assert(rewriter.labelStack.isEmpty)
+  }
+
+  it should "reuse labels already present" in {
+    val before = {
+      val switchLabel = new LabelDecl()
+      Block(Seq(
+        Label(switchLabel),
+        Switch(
+          Constant.IntegerValue(5),
+          Block(Seq(
+            Case(Constant.IntegerValue(0)),
+            Break(None),
+            Case(Constant.IntegerValue(5)),
+            Break(None),
+          ))
+        )
+      ))
+    }
+
+    val after = {
+      val switchLabel = new LabelDecl()
+      Block(Seq(
+        Label(switchLabel),
+        Switch(
+          Constant.IntegerValue(5),
+          Block(Seq(
+            Case(Constant.IntegerValue(0)),
+            Break(Some(switchLabel.ref)),
+            Case(Constant.IntegerValue(5)),
+            Break(Some(switchLabel.ref)),
+          ))
+        )
+      ))
+    }
+
+    val rewriter = SpecifyImplicitLabels()
+    RewriteTestHelper.test(
+      rewriter,
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(before),
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(after)
+    )
+    assert(rewriter.labelStack.isEmpty)
+  }
+
+  it should "not touch labeled break" in {
+    val before = {
+      val switchLabel = new LabelDecl()
+      Block(Seq(
+        Label(switchLabel),
+        Switch(
+          Constant.IntegerValue(5),
+          Block(Seq(
+            Case(Constant.IntegerValue(0)),
+            Break(Some(switchLabel.ref)),
+          ))
+        )
+      ))
+    }
+
+    val after = {
+      val switchLabel = new LabelDecl()
+      Block(Seq(
+        Label(switchLabel),
+        Switch(
+          Constant.IntegerValue(5),
+          Block(Seq(
+            Case(Constant.IntegerValue(0)),
+            Break(Some(switchLabel.ref)),
+          ))
+        )
+      ))
+    }
+
+    val rewriter = SpecifyImplicitLabels()
+    RewriteTestHelper.test(
+      rewriter,
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(before),
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(after)
+    )
+    assert(rewriter.labelStack.isEmpty)
+  }
+
+  it should "use the nearest label possible" in {
+    val before = {
+      val switchLabelA = new LabelDecl()
+      Block(Seq(
+        Label(switchLabelA),
+        Switch(
+          Constant.IntegerValue(33),
+          Switch(
+            Constant.IntegerValue(5),
+            Block(Seq(
+              Case(Constant.IntegerValue(0)),
+              Break(Some(switchLabelA.ref)),
+              Case(Constant.IntegerValue(5)),
+              Break(None),
+            ))
+          ))
+        )
+      )
+    }
+
+    val after = {
+      val switchLabelA = new LabelDecl()
+      val switchLabelB = new LabelDecl()
+      Block(Seq(
+        Label(switchLabelA),
+        Switch(
+          Constant.IntegerValue(33),
+          Block(Seq(
+            Label(switchLabelB),
+            Switch(
+              Constant.IntegerValue(5),
+              Block(Seq(
+                Case(Constant.IntegerValue(0)),
+                Break(Some(switchLabelA.ref)),
+                Case(Constant.IntegerValue(5)),
+                Break(Some(switchLabelB.ref))
+              ))
+            )
+          ))
+        )
+      ))
+    }
+
+    val rewriter = SpecifyImplicitLabels()
+    RewriteTestHelper.test(
+      rewriter,
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(before),
+      SimpleProgramGenerator.generateProgramWithSingleClassAndSingleMethod(after)
+    )
+    assert(rewriter.labelStack.isEmpty)
+  }
+}
