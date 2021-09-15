b90bee6c1b351e4239ba1bcf2c3233c7136392af
Bob Rubbens <bobrubbens@gmail.com>
2021-09-15 11:40:53 +0200

    Refactor ContinueToBreak
    - Use getOrElseUpdate instead of updating the map manually
    - Derive preferredName from the given origin instead of from an argument in ContinueToBreakOrigin
    - Use dispatch instead of rewriteDeafult for loop.body

--- src/main/java/vct/col/newrewrite/ContinueToBreak.scala
+++ src/main/java/vct/col/newrewrite/ContinueToBreak.scala
@@ -6,9 +6,12 @@ import vct.col.ast._
 
 import scala.collection.mutable
 
-case class ContinueToBreakOrigin(loopLabel: String, inner: Origin) extends Origin {
-  override def preferredName: String = "continue" + loopLabel.capitalize
-  override def messageInContext(message: String): String = inner.messageInContext(message)
+// We don't include the origin of the Continue statement here, because that doesn't make sense if there are
+// multiple continue statements referring to the same label. In that case, we only refer to label origin, as
+// that is what all continues have in common. This should be changed if it results in a bad user experience.
+case class ContinueToBreakOrigin(labelDeclOrigin: Origin) extends Origin {
+  override def preferredName: String = "continue" + labelDeclOrigin.preferredName.capitalize
+  override def messageInContext(message: String): String = labelDeclOrigin.messageInContext(message)
 }
 
 case class ContinueToBreak() extends Rewriter {
@@ -16,13 +19,13 @@ case class ContinueToBreak() extends Rewriter {
 
   override def dispatch(stat: Statement): Statement = stat match {
     case block@Block(Seq(l@Label(labelDecl), loop: Loop)) =>
-      val rewrittenBody = rewriteDefault(loop.body)
+      val rewrittenBody = dispatch(loop.body)
 
       // If the loop label appears in the mapping, it means it contains some continue that wants to break
       // from the inner label. We create a block wrapping the body, labeled with the inner label.
       val possiblyWrappedBody = loopLabelToInnerLabel.get(labelDecl) match {
         case Some(innerLabelDecl) =>
-          implicit val o = loop.o
+          implicit val o = innerLabelDecl.o
           Block(Seq(Label(innerLabelDecl), rewrittenBody))
         case None => rewrittenBody
       }
@@ -31,14 +34,10 @@ case class ContinueToBreak() extends Rewriter {
 
     case c@Continue(Some(Ref(labelDecl: LabelDecl))) =>
       // Reuse an already created inner label or create one
-      val innerLabelDecl = loopLabelToInnerLabel.get(labelDecl) match {
-        case Some(l) => l
-        case None =>
-          val innerLabelDecl = new LabelDecl()(ContinueToBreakOrigin(labelDecl.o.preferredName, labelDecl.o))
-          loopLabelToInnerLabel.put(labelDecl, innerLabelDecl)
-          innerLabelDecl
-      }
-
+      val innerLabelDecl = loopLabelToInnerLabel.getOrElseUpdate(
+        labelDecl,
+        new LabelDecl()(ContinueToBreakOrigin(labelDecl.o))
+      )
       Break(Some(innerLabelDecl.ref))(c.o)
 
     case other => rewriteDefault(other)
