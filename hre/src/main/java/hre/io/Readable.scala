ee3337a13f8cebeb8d52523b1116cdab42fd8856
Pieter Bos <p.h.bos@student.utwente.nl>
2022-03-25 15:19:52 +0100

    stupid lookbehind nonsense to include first line if empty

--- hre/src/main/java/hre/io/Readable.scala
+++ hre/src/main/java/hre/io/Readable.scala
@@ -29,11 +29,18 @@ trait Readable {
     read { reader =>
       val scanner = new Scanner(reader)
       // PB: Bit of a guess, but I think this is the line numbering ANTLR uses, which we want to match for origins.
-      scanner.useDelimiter("\n")
+      scanner.useDelimiter("(?<!\\A)\n")
       val buf = mutable.ArrayBuffer[String]()
       while(scanner.hasNext) {
-        buf += scanner.next()
+        val next = scanner.next()
+        if(next.nonEmpty && next.charAt(0) == '\n') {
+          buf += ""
+          buf += next.substring(1)
+        } else {
+          buf += next
+        }
       }
+      println(buf.map(_.replace("\n", raw"\n")))
       buf.toSeq
     }
 }
