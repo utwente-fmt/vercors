d5dcc4574923dc26061594c87750591feca7e8a7
Bob Rubbens <bobrubbens@gmail.com>
2022-04-13 15:40:46 +0200

    Add test for calling static methods

---
+++ src/main/universal/res/jdk/java/lang/Math.java
@@ -0,0 +1,18 @@
+package java.lang;
+
+class Math {
+    /*@
+    ghost
+    public static pure double sqrt(double x) {
+        // Blatantly wrong!
+        return x;
+    }
+
+    // The standard doesn't have this one, but since int literals are not yet cast to doubles automatically, this one is in Math too.
+    ghost
+    public static pure int sqrt(int x) {
+        // Blatantly wrong!
+        return x;
+    }
+    */
+}
