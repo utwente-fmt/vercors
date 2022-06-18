41715c5fb188513a0b8df272ae70ba34bd95e617
Bob Rubbens <bobrubbens@gmail.com>
2022-06-18 15:28:01 +0200

    Use collectInScope instead of custom someNodes

--- src/main/java/vct/col/newrewrite/ExtractInlineQuantifierPatterns.scala
+++ src/main/java/vct/col/newrewrite/ExtractInlineQuantifierPatterns.scala
@@ -3,6 +3,9 @@ package vct.col.newrewrite
 import vct.col.ast._
 import vct.col.rewrite.{Generation, Rewriter, RewriterBuilder}
 import RewriteHelpers._
+import hre.util.ScopedStack
+
+import scala.collection.mutable.ArrayBuffer
 
 case object ExtractInlineQuantifierPatterns extends RewriterBuilder {
   override def key: String = "inlineQuantifierPattern"
@@ -10,50 +13,31 @@ case object ExtractInlineQuantifierPatterns extends RewriterBuilder {
 }
 
 case class ExtractInlineQuantifierPatterns[Pre <: Generation]() extends Rewriter[Pre] {
-  def someTransSubnodes[G](x: Node[G], P: Node[G] => Boolean): LazyList[Node[G]] = {
-    if (P(x)) {
-      x #:: Subnodes.subnodes(x).to(LazyList).flatMap(someTransSubnodes(_, P))
-    } else {
-      LazyList()
-    }
-  }
-
-  def notForall[G](x: Node[G]): Boolean = x match {
-    case _: Forall[G] => false
-    case _ => true
-  }
-
-  def refersVars[G](e: Expr[G]): LazyList[Variable[G]] =
-    e.transSubnodes.collect({ case Local(r) => r.decl })
-
-  def getInlineTriggers[G](bindings: Seq[Variable[G]], e: Expr[G]): Seq[InlinePattern[G]] =
-    someTransSubnodes(e, notForall[G]).collect({ case it: InlinePattern[G] => it })
-      // Only keep inline patterns that refer to at least _one_ quantified variable
-      .filter(ipat => refersVars(ipat).exists(bindings.contains(_)))
-      .toIndexedSeq
+  val patterns: ScopedStack[ArrayBuffer[InlinePattern[Pre]]] = ScopedStack()
 
   override def dispatch(e: Expr[Pre]): Expr[Post] = e match {
-    case i: InlinePattern[Pre] => dispatch(i.inner)
-    case f: Forall[Pre] =>
-      if (f.triggers.nonEmpty) {
-        rewriteDefault(f)
-      } else {
-        val triggers = getInlineTriggers(f.bindings, f.body).map(t => dispatch(t)) match {
-          case Nil => Seq()
-          case ts => Seq(ts)
-        }
-        f.rewrite(triggers = triggers)
+    case i: InlinePattern[Pre] =>
+      patterns.topOption.foreach { _ += i }
+      dispatch(i.inner)
+
+    case f: Forall[Pre] if f.triggers.isEmpty =>
+      val (patternsHere, body) = withCollectInScope(patterns) {
+        dispatch(f.body)
       }
-    case f: Starall[Pre] =>
-      if (f.triggers.nonEmpty) {
-        rewriteDefault(f)
-      } else {
-        val triggers = getInlineTriggers(f.bindings, f.body).map(t => dispatch(t)) match {
-          case Nil => Seq()
-          case ts => Seq(ts)
-        }
-        f.rewrite(triggers = triggers)
+      f.rewrite(triggers = Seq(patternsHere.map(dispatch)), body = body)
+
+    case f: Starall[Pre] if f.triggers.isEmpty =>
+      val (patternsHere, body) = withCollectInScope(patterns) {
+        dispatch(f.body)
       }
+      f.rewrite(triggers = Seq(patternsHere.map(dispatch)), body = body)
+
+    case f: Exists[Pre] if f.triggers.isEmpty =>
+      val (patternsHere, body) = withCollectInScope(patterns) {
+        dispatch(f.body)
+      }
+      f.rewrite(triggers = Seq(patternsHere.map(dispatch)), body = body)
+
     case other => rewriteDefault(other)
   }
 }
