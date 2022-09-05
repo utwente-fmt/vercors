fef4c9461b15a990c7ae5a8fbb5787e69f125bf1
Bob Rubbens <bobrubbens@gmail.com>
2022-09-05 11:59:13 +0200

    Missing test

---
+++ examples/technical/javabip/bipGuardIsUsed.java
@@ -0,0 +1,30 @@
+package org.javabip.spec.deviation;
+
+import org.javabip.annotations.*;
+import org.javabip.api.PortType;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@ComponentType(initial = INIT, name = NAME)
+@StatePredicate(state = "xyz", expr = "true")
+public class GuardIsUsed {
+    public static final String INIT = "initialState";
+    public static final String NAME = "oneComponentOneTransition";
+    public static final String GO = "go";
+    public static final String GEQ_ZERO = "greaterEqualZero";
+
+    private int x;
+
+    @Transition(name = GO, source = INIT, target = INIT, guard = GEQ_ZERO)
+    public void goTransition() {
+        //@ assert x >= 0;
+    }
+
+    @Pure
+    @Guard(name = GEQ_ZERO)
+    public boolean geqZero() {
+        return x >= 0;
+    }
+}
+
