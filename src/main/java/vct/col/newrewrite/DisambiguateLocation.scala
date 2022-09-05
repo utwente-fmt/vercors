d49b87bc3f3490a14b9eb52f848b3f1e0ea10749
Pieter Bos <p.h.bos@student.utwente.nl>
2022-09-05 20:46:14 +0200

    translate array, pointer locations in ImportADT, fixes #773

--- src/main/java/vct/col/newrewrite/DisambiguateLocation.scala
+++ src/main/java/vct/col/newrewrite/DisambiguateLocation.scala
@@ -1,8 +1,8 @@
 package vct.col.newrewrite
 
-import vct.col.ast.{AmbiguousLocation, ArrayLocation, ArraySubscript, Deref, Expr, FieldLocation, InstancePredicateApply, InstancePredicateLocation, Location, ModelDeref, ModelLocation, PointerLocation, PredicateApply, PredicateLocation, SilverDeref, SilverFieldLocation, WritePerm}
+import vct.col.ast._
 import vct.col.newrewrite.DisambiguateLocation.NotALocation
-import vct.col.origin.Origin
+import vct.col.origin.{Blame, Origin, PointerLocationError}
 import vct.col.rewrite.{Generation, Rewriter, RewriterBuilder}
 import vct.result.VerificationError.UserError
 
@@ -19,30 +19,30 @@ case object DisambiguateLocation extends RewriterBuilder {
 }
 
 case class DisambiguateLocation[Pre <: Generation]() extends Rewriter[Pre]  {
-  override def dispatch(loc: Location[Pre]): Location[Post] = {
-    implicit val o: Origin = loc.o
-    loc match {
-      case location@AmbiguousLocation(expr) => {
-        expr match {
-          case Deref(obj, ref) =>
-            FieldLocation(dispatch(obj), succ(ref.decl))
-          case ModelDeref(obj, ref) =>
-            ModelLocation(dispatch(obj), succ(ref.decl))
-          case SilverDeref(obj, ref) =>
-            SilverFieldLocation(dispatch(obj), succ(ref.decl))
-          case expr@ArraySubscript(arr, index) =>
-            ArrayLocation(dispatch(arr), dispatch(index))(expr.blame)
-          case point@expr if expr.t.asPointer.isDefined =>
-            PointerLocation(dispatch(expr))(location.blame)
-          case PredicateApply(ref, args, WritePerm()) =>
-            PredicateLocation(succ(ref.decl), (args.map(dispatch)))
-          case InstancePredicateApply(obj, ref, args, WritePerm()) =>
-            InstancePredicateLocation(succ(ref.decl), dispatch(obj), args.map(dispatch))
-          case default =>
-            throw NotALocation(default)
-        }
-      }
-      case other => rewriteDefault(other)
-    }
+  def exprToLoc(expr: Expr[Pre], blame: Blame[PointerLocationError])(implicit o: Origin): Location[Post] = expr match {
+    case Deref(obj, ref) =>
+      FieldLocation(dispatch(obj), succ(ref.decl))
+    case ModelDeref(obj, ref) =>
+      ModelLocation(dispatch(obj), succ(ref.decl))
+    case SilverDeref(obj, ref) =>
+      SilverFieldLocation(dispatch(obj), succ(ref.decl))
+    case expr@ArraySubscript(arr, index) =>
+      ArrayLocation(dispatch(arr), dispatch(index))(expr.blame)
+    case expr if expr.t.asPointer.isDefined =>
+      PointerLocation(dispatch(expr))(blame)
+    case PredicateApply(ref, args, WritePerm()) =>
+      PredicateLocation(succ(ref.decl), (args.map(dispatch)))
+    case InstancePredicateApply(obj, ref, args, WritePerm()) =>
+      InstancePredicateLocation(succ(ref.decl), dispatch(obj), args.map(dispatch))
+
+    case InlinePattern(inner) => exprToLoc(inner, blame)
+
+    case default =>
+      throw NotALocation(default)
+  }
+
+  override def dispatch(loc: Location[Pre]): Location[Post] = loc match {
+    case location@AmbiguousLocation(expr) => exprToLoc(expr, location.blame)(loc.o)
+    case other => rewriteDefault(other)
   }
 }
