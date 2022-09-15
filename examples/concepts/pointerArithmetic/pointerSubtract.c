430afd528e46abc22b1dbb9f3541f048a52d5054
naum_tomov <n.tomov@student.utwente.nl>
2022-09-15 15:38:25 +0200

    fixed verification problem by loosening the trigger in option.pvl for `some`.

--- examples/concepts/pointerArithmetic/pointerSubtract.c
+++ examples/concepts/pointerArithmetic/pointerSubtract.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-// FAILS TO VERIFY CURRENTLY DUE TO WRONG POINTER AXIOMS
 int main(){
 }
 
 /*@
 requires ptr != NULL;
-requires \pointer_block_length(ptr) == 4 && \pointer_block_offset(ptr) == 3;
-ensures (ptr - 1) + 1 == ptr;
+requires \pointer_block_length(ptr) == 5 && \pointer_block_offset(ptr) == 3;
+ensures (ptr - 1) - 1 != ptr;
 @*/
 void test(int *ptr) {
-
+    int x = 0;
+    //@ assert false;
+    return;
 }
