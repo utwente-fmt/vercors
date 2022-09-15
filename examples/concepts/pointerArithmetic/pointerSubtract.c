4b8cc6df75f9a2f65d18ab6fbf890026af07462f
naum_tomov <n.tomov@student.utwente.nl>
2022-09-15 15:39:00 +0200

    fixed formatting of the  pointerSubtract.c test example;

--- examples/concepts/pointerArithmetic/pointerSubtract.c
+++ examples/concepts/pointerArithmetic/pointerSubtract.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
+
 int main(){
 }
-
 /*@
 requires ptr != NULL;
 requires \pointer_block_length(ptr) == 5 && \pointer_block_offset(ptr) == 3;
@@ -9,6 +9,5 @@ ensures (ptr - 1) - 1 != ptr;
 @*/
 void test(int *ptr) {
     int x = 0;
-    //@ assert false;
     return;
 }
