db96a5d526e352d2ea3070be0ee519a4068b1908
Bob Rubbens <bobrubbens@gmail.com>
2022-10-11 10:21:05 +0200

    Add static method to System to encode non-null knowledge. Reinstate temporarily disabled tests

--- src/test/scala/vct/test/integration/examples/TechnicalJavaSpec.scala
+++ src/test/scala/vct/test/integration/examples/TechnicalJavaSpec.scala
@@ -3,13 +3,14 @@ package vct.test.integration.examples
 import vct.test.integration.helper.VercorsSpec
 
 class TechnicalJavaSpec extends VercorsSpec {
-//  vercors should verify using silicon example "technical/java/JavaAnnotation.java"
-//  vercors should verify using silicon example "technical/java/JavaString.java"
+  vercors should verify using silicon example "technical/java/JavaAnnotation.java"
+  vercors should verify using silicon example "technical/java/JavaString.java"
 
   vercors should verify using silicon in "java.lang classes should be reachable without imports" java
 """
 class C {
   int m() {
+    //@ ghost System.staticInvariant();
     System.out.println("Hello world!");
     return Math.sqrt(2);
   }
