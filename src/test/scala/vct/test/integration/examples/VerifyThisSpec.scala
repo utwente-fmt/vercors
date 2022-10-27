8c6e01f9523c25db7d38d96a8760f2d9647fc70e
Pieter Bos <p.h.bos@student.utwente.nl>
2022-10-27 15:50:53 +0200

    update verifythis paths

--- src/test/scala/vct/test/integration/examples/VerifyThisSpec.scala
+++ src/test/scala/vct/test/integration/examples/VerifyThisSpec.scala
@@ -3,7 +3,7 @@ package vct.test.integration.examples
 import vct.test.integration.helper.VercorsSpec
 
 class VerifyThisSpec extends VercorsSpec {
-  vercors should verify using silicon example "verifythis/prefix.pvl"
-  vercors should verify using silicon example "verifythis/lcp.pvl"
+  vercors should verify using silicon example "verifythis/2015/relaxed_prefix.pvl"
+  vercors should verify using silicon example "verifythis/2012/lcp.pvl"
   vercors should verify using silicon example "verifythis/2019/challenge1.pvl"
 }
