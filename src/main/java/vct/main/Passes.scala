ea331c0bf7a64e2313b018efa98d36ea4ced77eb
Petra van den Bos <p.vandenbos@utwente.nl>
2022-11-18 13:03:33 +0100

    restore deleted imports

--- src/main/java/vct/main/Passes.scala
+++ src/main/java/vct/main/Passes.scala
@@ -2,9 +2,20 @@ package vct.main
 
 import hre.config.Configuration
 
+import java.io.{File, FileNotFoundException, FileOutputStream, IOException, PrintWriter}
+import hre.lang.System.{Abort, Debug, Fail}
+import vct.col.ast.stmt.decl.{ASTClass, ASTSpecial, ProgramUnit}
+import vct.col.ast.syntax.{JavaDialect, JavaSyntax, PVLSyntax}
 import vct.col.features
 import vct.col.features.Feature
 import vct.col.rewrite._
+import vct.col.util.{JavaTypeCheck, LocalVariableChecker}
+import vct.col.veymont.{Decompose, GenerateForkJoinMain, GlobalProgPerms, JavaForkJoin, LocalProgConstructors, PrintVeyMontProg, StructureCheck, TerminationCheck, Util}
+import vct.experiments.learn.{NonLinCountVisitor, Oracle}
+import vct.logging.{ExceptionMessage, PassReport}
+import vct.parsers.rewrite.{AnnotationInterpreter, ConvertTypeExpressions, EncodeAsClass, FilterSpecIgnore, FlattenVariableDeclarations, InferADTTypes, RewriteWithThen, StripUnusedExtern}
+
+import scala.jdk.CollectionConverters._
 import vct.col.veymont.{ Decompose, GenerateForkJoinMain,  GlobalProgPerms,  LocalProgConstructors, PrintVeyMontProg, StructureCheck, TerminationCheck, Util}
 
 object Passes {
