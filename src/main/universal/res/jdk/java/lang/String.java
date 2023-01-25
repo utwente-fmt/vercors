7b0da290b020a14f6a6acf889562402105dd7165
Bob Rubbens <bobrubbens@gmail.com>
2023-01-25 14:35:13 +0100

    Add proper termination measures to jdk string lib code

--- src/main/universal/res/jdk/java/lang/String.java
+++ src/main/universal/res/jdk/java/lang/String.java
@@ -3,24 +3,31 @@ package java.lang;
 // TODO: Should not have to use instanceof anywhere in this file
 /*@
 ghost
+decreases assume;
 ensures \result != null ** \result instanceof String ** \result.data() == data;
 ensures (\forall string otherData; true; (otherData == data) == (\result == internToString(otherData)));
 pure String internToString(string data);
 
 ghost
+decreases;
 ensures \result != null ** \result instanceof String;
 pure String concatStrings(String a, String b);
 @*/
 
 public class String {
     /*@
+    decreases;
     pure string data();
     */
 
+    //@ decreases;
     //@ ensures \result == internToString(data());
     public native /*@ pure */ String intern();
 
     /*@
-    ghost public pure boolean isEmpty();
+
+    ghost
+    decreases;
+    public pure boolean isEmpty();
      @*/
 }
