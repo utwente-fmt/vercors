57edc9ce7a4836eb02fc330f9a2965d2bd360d8f
naum_tomov <n.tomov@student.utwente.nl>
2023-02-01 16:03:00 +0100

    added it to the coverage

--- src/test/scala/vct/test/integration/meta/ExampleCoverage.scala
+++ src/test/scala/vct/test/integration/meta/ExampleCoverage.scala
@@ -1,7 +1,7 @@
 package vct.test.integration.meta
 
 import org.scalatest.flatspec.AnyFlatSpec
-import vct.test.integration.examples.{AbruptExamplesSpec, AlgorithmExamplesSpec, ArrayExamplesSpec, BasicExamplesSpec, CIncludeSpec, ClassesSpec, CounterSpec, DemoSpec, ForkJoinSpec, GotoSpec, GpgpuSpec, JavaImportSpec, LoopDependencySpec, MapsSpec, ModelsSpec, OpenMPSpec, ParallelSpec, PermissionSpec, PermutationSpec, PointerSpec, PredicatesSpec, PublicationsSpec, RefuteSpec, SequencesSpec, SetsSpec, SilverDomainSpec, SummationSpec, TechnicalAbruptSpec, TechnicalFloatSpec, TechnicalSpec, TechnicalVeymontSpec, TerminationSpec, TypeValuesSpec, VerifyThisSpec, VeymontSpec, WaitNotifySpec, WandSpec}
+import vct.test.integration.examples.{AbruptExamplesSpec, AlgorithmExamplesSpec, ArrayExamplesSpec, BasicExamplesSpec, CIncludeSpec, ClassesSpec, CounterSpec, DemoSpec, ForkJoinSpec, GotoSpec, GpgpuSpec, JavaImportSpec, LoopDependencySpec, MapsSpec, ModelsSpec, OpenMPSpec, ParallelSpec, PermissionSpec, PermutationSpec, PointerSpec, PredicatesSpec, PublicationsSpec, RefuteSpec, SequencesSpec, SetsSpec, SilverDomainSpec, SummationSpec, TechnicalAbruptSpec, TechnicalFloatSpec, TechnicalSpec, TechnicalStaticSpec, TechnicalVeymontSpec, TerminationSpec, TypeValuesSpec, VerifyThisSpec, VeymontSpec, WaitNotifySpec, WandSpec}
 import vct.test.integration.helper.{ExampleFiles, VercorsSpec}
 
 class ExampleCoverage extends AnyFlatSpec {
@@ -37,6 +37,7 @@ class ExampleCoverage extends AnyFlatSpec {
       new TechnicalAbruptSpec(),
       new TechnicalFloatSpec(),
       new TechnicalSpec(),
+      new TechnicalStaticSpec(),
       new TechnicalVeymontSpec(),
       new TerminationSpec(),
       new TypeValuesSpec(),
