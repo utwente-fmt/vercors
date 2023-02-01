e3f763853fdc95f2cbb52570e2253a58701c8186
Bob Rubbens <bobrubbens@gmail.com>
2023-02-01 16:22:39 +0100

    Remvoe unused function. Add comment.

--- col/src/main/java/vct/col/ast/lang/JavaClassLiteralImpl.scala
+++ col/src/main/java/vct/col/ast/lang/JavaClassLiteralImpl.scala
@@ -2,6 +2,7 @@ package vct.col.ast.lang
 
 import vct.col.ast.{JavaClassLiteral, Type}
 
+// JavaBIP code needs an AST node for this concept
 trait JavaClassLiteralImpl[G] { this: JavaClassLiteral[G] =>
   def t: Type[G] = ???
 }
--- col/src/main/java/vct/col/ast/lang/JavaNamespaceImpl.scala
+++ col/src/main/java/vct/col/ast/lang/JavaNamespaceImpl.scala
@@ -5,5 +5,4 @@ import vct.col.origin.DiagnosticOrigin
 import vct.col.resolve.lang.Java
 
 trait JavaNamespaceImpl[G] { this: JavaNamespace[G] =>
-  def isJavaLang(): Boolean = pkg.map(_.names).contains(Java.JAVA_LANG)
 }
