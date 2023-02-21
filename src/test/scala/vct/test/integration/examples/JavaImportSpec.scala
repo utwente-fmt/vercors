10f95be27f689de83360ce30d2381bb8690632fa
naum_tomov <n.tomov@student.utwente.nl>
2023-02-21 15:36:16 +0100

    added forgotten examples

--- src/test/scala/vct/test/integration/examples/JavaImportSpec.scala
+++ src/test/scala/vct/test/integration/examples/JavaImportSpec.scala
@@ -4,5 +4,7 @@ import vct.test.integration.helper.VercorsSpec
 
 class JavaImportSpec extends VercorsSpec {
    vercors should verify using anyBackend example "concepts/import/src/separate/Main.java"
-   vercors should verify using anyBackend example "concepts/import/src/separate/Util.java"
+  vercors should verify using anyBackend example "concepts/import/src/separate/Util.java"
+  vercors should verify using anyBackend example "concepts/import/src/Sqrt.java"
+  vercors should verify using anyBackend example "concepts/import/src/SqrtStar.java"
 }
