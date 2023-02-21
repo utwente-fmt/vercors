41bda6a09299ca101d5c84ab01d3af5583599e16
naum_tomov <n.tomov@student.utwente.nl>
2023-02-21 15:40:41 +0100

    fix typo

--- src/test/scala/vct/test/integration/examples/JavaImportSpec.scala
+++ src/test/scala/vct/test/integration/examples/JavaImportSpec.scala
@@ -5,6 +5,6 @@ import vct.test.integration.helper.VercorsSpec
 class JavaImportSpec extends VercorsSpec {
    vercors should verify using anyBackend example "concepts/import/src/separate/Main.java"
   vercors should verify using anyBackend example "concepts/import/src/separate/Util.java"
-  vercors should verify using anyBackend example "concepts/import/src/Sqrt.java"
-  vercors should verify using anyBackend example "concepts/import/src/SqrtStar.java"
+  vercors should verify using anyBackend example "concepts/import/Sqrt.java"
+  vercors should verify using anyBackend example "concepts/import/SqrtStar.java"
 }
