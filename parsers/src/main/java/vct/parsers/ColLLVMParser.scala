36877f91973ae5cdea3e07365308a097b76509e7
Dré van Oorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2023-03-02 17:39:40 +0100

    [VCLLVM] implement COL buffer deserialization for LLVMParser

--- parsers/src/main/java/vct/parsers/ColLLVMParser.scala
+++ parsers/src/main/java/vct/parsers/ColLLVMParser.scala
@@ -3,11 +3,12 @@ package vct.parsers
 import com.typesafe.scalalogging.LazyLogging
 import hre.io.Readable
 import org.antlr.v4.runtime.CharStream
+import vct.col.ast.Deserialize
+import vct.col.serialize.{GlobalDeclaration, Program}
 import vct.parsers.transform.{BlameProvider, OriginProvider}
 import vct.result.VerificationError.{SystemError, Unreachable, UserError}
 
-import java.io.{InputStreamReader, OutputStreamWriter, StringWriter}
-import java.nio.charset.StandardCharsets
+import java.io.{InputStreamReader, StringWriter}
 
 case class ColLLVMParser(override val originProvider: OriginProvider, override val blameProvider: BlameProvider) extends Parser(originProvider, blameProvider) with LazyLogging {
   case class LLVMParseError(fileName: String, errorCode: Int, error: String) extends UserError {
@@ -17,16 +18,12 @@ case class ColLLVMParser(override val originProvider: OriginProvider, override v
       s"Parsing file $fileName failed with exit code $errorCode:\n$error"
   }
 
-  case class LLVMStubError(message: String) extends SystemError {
-    override def text: String = message
-  }
-
   override def parse[G](stream: CharStream): ParseResult[G] = {
     throw Unreachable("LLVM IR files should be parsed from an not ANTLR CharStream, use VCLLVM instead!")
   }
 
   override def parse[G](readable: Readable): ParseResult[G] = {
-    var command = Seq("VCLLVM", "--sample-col") //TODO convert --sample-col to actual file at some point
+    val command = Seq("VCLLVM", "--sample-col") //TODO convert --sample-col to actual file at some point
     val process = new ProcessBuilder(command:_*).start()
     process.waitFor()
     if (process.exitValue() != 0) {
@@ -36,12 +33,9 @@ case class ColLLVMParser(override val originProvider: OriginProvider, override v
       writer.close()
       throw LLVMParseError(readable.fileName, process.exitValue(), writer.toString)
     }
-    //TODO implement Protobuf -> ParseResult
-    val writer = new StringWriter()
-    new InputStreamReader(process.getInputStream).transferTo(writer)
-    writer.close()
-    val buffer = writer.toString
-    throw LLVMStubError(s"Protobuf interpreter not yet implemented. Dumping buffer...\n$buffer")
+    val protoProgram = Program.parseFrom(process.getInputStream)
+    val COLProgram = Deserialize.deserialize[G](protoProgram)
+    ParseResult(COLProgram.declarations, Seq.empty)
   }
 
 }
