028b64d203ad49c5cb9472d976905ecdd225a01b
Pieter Bos <p.h.bos@utwente.nl>
2023-04-12 12:16:40 +0200

    run contract empty

--- src/col/vct/col/ast/declaration/cls/RunMethodImpl.scala
+++ src/col/vct/col/ast/declaration/cls/RunMethodImpl.scala
@@ -5,6 +5,8 @@ import vct.col.print._
 
 trait RunMethodImpl[G] { this: RunMethod[G] =>
   override def layout(implicit ctx: Ctx): Doc =
-    contract.show <+/>
-    "run" <> body.map(Text(" ") <> _.layoutAsBlock).getOrElse(Text(";"))
+    Doc.stack(Seq(
+      contract.show,
+      Text("run") <> body.map(Empty <+> _.layoutAsBlock).getOrElse(Text(";")),
+    ))
 }
