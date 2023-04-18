79f1a5079dcebee5024f92a3bb3c048263a2f7fe
Armborst <l.armborst@utwente.nl>
2023-04-18 16:48:33 +0200

    small fixes to Windows scripts for invoking Viper

--- bin/silicon.cmd
+++ bin/silicon.cmd
@@ -5,9 +5,9 @@ set BIN=%~dp0
 set ROOT=%BIN%..
 
 pushd %ROOT%
-mill vercors.runScript
+call mill vercors.runScript
 popd
 
-set Z3="%ROOT%res\universal\deps\win\z3\bin\z3.exe"
+set Z3="%ROOT%\res\universal\deps\win\z3\bin\z3.exe"
 
-call "%ROOT%out\vercors\runScript.dest\silicon" --z3Exe %Z3% %*
+call "%ROOT%\out\vercors\runScript.dest\silicon" --z3Exe %Z3% %*
