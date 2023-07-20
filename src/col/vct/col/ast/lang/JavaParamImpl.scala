07bd995d1db0036d09458a37b60bfdf7da32d1b2
Petra van den Bos <p.vandenbos@utwente.nl>
2023-07-20 14:17:25 +0200

    layout fixes

--- src/col/vct/col/ast/lang/JavaParamImpl.scala
+++ src/col/vct/col/ast/lang/JavaParamImpl.scala
@@ -4,6 +4,6 @@ import vct.col.ast.JavaParam
 import vct.col.print.{Ctx, Doc, Text}
 
 trait JavaParamImpl[G] { this: JavaParam[G] =>
-  override def layout(implicit ctx: Ctx): Doc = Text(t + " " + name)
+  override def layout(implicit ctx: Ctx): Doc = t.show <+> name
 
 }
--- src/col/vct/col/ast/type/TUnionImpl.scala
+++ src/col/vct/col/ast/type/TUnionImpl.scala
@@ -4,6 +4,6 @@ import vct.col.ast.TUnion
 import vct.col.print.{Ctx, Doc}
 
 trait TUnionImpl[G] { this: TUnion[G] =>
-  override def layout(implicit ctx: Ctx): Doc =  Doc.spread(types)
+  override def layout(implicit ctx: Ctx): Doc =  Doc.fold(types)(_ <+> "|" <+> _)
 
 }
