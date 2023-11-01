b7a915caefa459a23dbb9776a8e3615e6aa86463
Bob Rubbens <bobrubbens@gmail.com>
2023-11-01 10:31:07 +0100

    Remove infinite recursion

--- src/col/vct/col/ast/statement/exceptional/EvalImpl.scala
+++ src/col/vct/col/ast/statement/exceptional/EvalImpl.scala
@@ -1,10 +1,11 @@
 package vct.col.ast.statement.exceptional
 
+import vct.col.ast.statement.StatementImpl
 import vct.col.ast.{EndpointUse, Eval, MethodInvocation, Statement, ThisSeqProg}
 import vct.col.check.{CheckContext, CheckError, SeqProgInvocation}
 import vct.col.print.{Ctx, Doc}
 
-trait EvalImpl[G] { this: Eval[G] =>
+trait EvalImpl[G] extends StatementImpl[G] { this: Eval[G] =>
   override def layout(implicit ctx: Ctx): Doc = expr.show <> ";"
 
   override def enterCheckContext(context: CheckContext[G]): CheckContext[G] = this match {
@@ -13,7 +14,7 @@ trait EvalImpl[G] { this: Eval[G] =>
     case _ => context
   }
 
-  override def check(context: CheckContext[G]): Seq[CheckError] = this.asInstanceOf[Statement[G]].check(context) ++ (context.currentSeqProg match {
+  override def check(context: CheckContext[G]): Seq[CheckError] = super.check(context) ++ (context.currentSeqProg match {
     case None => Seq()
     case Some(_) => this.expr match {
       case MethodInvocation(EndpointUse(_), _, _, _, _, _, _) => Seq()
