5661056a0b54c8fa3c1a3b030755b6946c922d62
Bob Rubbens <bobrubbens@gmail.com>
2023-11-22 10:54:14 +0100

    Rename impls

--- src/col/vct/col/ast/family/pvlcommunicate/PVLSubjectImpl.scala
+++ src/col/vct/col/ast/family/pvlcommunicate/PVLSubjectImpl.scala
@@ -3,7 +3,7 @@ package vct.col.ast.family.pvlcommunicate
 import vct.col.ast.{PVLSubject, TClass, Class, Type}
 import vct.col.resolve.ctx.RefPVLEndpoint
 
-trait PVLCommunicateSubjectImpl[G] { this: PVLSubject[G] =>
+trait PVLSubjectImpl[G] { this: PVLSubject[G] =>
   def cls: Class[G] = ref.get.decl.cls.decl
   def ref: Option[RefPVLEndpoint[G]]
 }
