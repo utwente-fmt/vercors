8348f4caaf9d6485ca298e5a53a7294897cc6af7
Lars <l.b.vandenhaak@gmail.com>
2023-11-24 00:12:05 +0100

    Duplicate test name

--- test/main/vct/test/integration/examples/CSpec.scala
+++ test/main/vct/test/integration/examples/CSpec.scala
@@ -326,7 +326,7 @@ class CSpec extends VercorsSpec {
     }
     """
 
-  vercors should fail withCode "copyStructFailed" using silicon in "Insufficient permission for field x to copy struct before call" c
+  vercors should fail withCode "copyStructFailed" using silicon in "Insufficient permission for field x to copy struct" c
     """
     struct d {
         int x;
