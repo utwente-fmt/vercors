e5430c1edf9f7d73aa2bac98a6bb069243941961
Bob Rubbens <bobrubbens@gmail.com>
2023-12-13 17:12:50 +0100

    Remove performance tests

--- test/main/vct/test/integration/examples/VeyMontToolPaperSpec.scala
+++ test/main/vct/test/integration/examples/VeyMontToolPaperSpec.scala
@@ -6,7 +6,6 @@ class VeyMontToolPaperSpec extends VercorsSpec {
   val wd = "publications/2023/VeyMontToolPaper"
   val applicability = s"$wd/applicability"
   val paperExamples = s"$wd/paper-examples"
-  val performance = s"$wd/performance"
 
   vercors should verify using silicon flag "--veymont-generate-permissions" example s"$applicability/2pc-3.pvl"
   vercors should verify using silicon flag "--veymont-generate-permissions" example s"$applicability/2pc-5.pvl"
@@ -16,14 +15,6 @@ class VeyMontToolPaperSpec extends VercorsSpec {
   vercors should verify using silicon flag "--veymont-generate-permissions" example s"$applicability/election-5.pvl"
 
   vercors should verify using silicon flag "--veymont-generate-permissions" example s"$paperExamples/veymont-swap.pvl"
+  // To be enabled when the trigger concretization bug w.r.t. inline predicates is fixed
 //  vercors should verify using silicon example s"$paperExamples/veymont-tictactoemn.pvl"
-
-  vercors should verify using silicon example s"$performance/binary-trees-2.pvl"
-  vercors should verify using silicon example s"$performance/binary-trees-4.pvl"
-  vercors should verify using silicon example s"$performance/binary-trees-6.pvl"
-  vercors should verify using silicon example s"$performance/binary-trees-8.pvl"
-  vercors should verify using silicon example s"$performance/k-nucleotide-1.pvl"
-  vercors should verify using silicon example s"$performance/k-nucleotide-2.pvl"
-  vercors should verify using silicon example s"$performance/k-nucleotide-3.pvl"
-  vercors should verify using silicon example s"$performance/k-nucleotide-4.pvl"
 }
