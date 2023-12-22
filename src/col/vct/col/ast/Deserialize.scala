e73b1016d3a3b573a17a3340bed19047555a9354
Pieter Bos <p.h.bos@utwente.nl>
2023-12-22 14:33:27 +0100

    serialize stuff

--- src/col/vct/col/ast/Deserialize.scala
+++ src/col/vct/col/ast/Deserialize.scala
@@ -1,5 +1,11 @@
 package vct.col.ast
 
+import vct.col.ast.ops.deserialize.DeserializeProgram
+
+import scala.collection.mutable
+
 object Deserialize {
-  def deserializeProgram[G](x: scala.Any, y: scala.Any): Program[G] = ???
+  def deserializeProgram[G](program: vct.col.ast.serialize.Program, y: scala.Any): Program[G] = {
+    DeserializeProgram.deserialize(program, mutable.HashMap())
+  }
 }
--- src/col/vct/col/ast/Serialize.scala
+++ src/col/vct/col/ast/Serialize.scala
@@ -1,5 +1,13 @@
 package vct.col.ast
 
 object Serialize {
-  def serialize(x: scala.Any): vct.col.serialize.Program = ???
+  def serialize[G](x: Program[G]): vct.col.ast.serialize.Program = {
+    val declarations = x.collect {
+      case decl: Declaration[G] => decl
+    }.zipWithIndex.map {
+      case (decl, id) => decl -> id.toLong
+    }.toMap
+
+    x.serializeFamily(declarations)
+  }
 }
