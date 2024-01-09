35905a6aba10ef5f68def5f423c148ce2ce50f37
Ellen Wittingen <e.m.wittingen@student.utwente.nl>
2024-01-09 16:53:38 +0100

    moved literalarray file to conform to refactor on dev

--- src/col/vct/col/ast/lang/cpp/CPPLiteralArrayImpl.scala
+++ src/col/vct/col/ast/lang/cpp/CPPLiteralArrayImpl.scala
@@ -1,10 +1,11 @@
-package vct.col.ast.lang
+package vct.col.ast.lang.cpp
 
 import vct.col.ast.{CPPLiteralArray, TNotAValue, Type}
 import vct.col.print.Doc.{arg, fold}
 import vct.col.print.{Ctx, Doc, Precedence, Text}
+import vct.col.ast.ops.CPPLiteralArrayOps
 
-trait CPPLiteralArrayImpl[G] { this: CPPLiteralArray[G] =>
+trait CPPLiteralArrayImpl[G] extends CPPLiteralArrayOps[G] { this: CPPLiteralArray[G] =>
   override def t: Type[G] = new TNotAValue()
 
   override def precedence = Precedence.ATOMIC
