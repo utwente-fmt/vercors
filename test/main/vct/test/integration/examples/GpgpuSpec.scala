7aa1f4e5dd8e9bb336056279048fbe1d91e72233
Lars <l.b.vandenhaak@gmail.com>
2024-03-27 11:52:42 +0100

    Add global fence example

--- test/main/vct/test/integration/examples/GpgpuSpec.scala
+++ test/main/vct/test/integration/examples/GpgpuSpec.scala
@@ -5,10 +5,13 @@ import vct.test.integration.helper.VercorsSpec
 class GpgpuSpec extends VercorsSpec {
   vercors should verify using silicon example "concepts/gpgpu/cuda.cu"
   vercors should verify using silicon example "concepts/gpgpu/cuda_blur.cu"
+
   vercors should verify using silicon example "concepts/gpgpu/dynamic_shared_cuda.cu"
   vercors should verify using silicon example "concepts/gpgpu/dynamic_shared_opencl.cl"
   vercors should verify using silicon example "concepts/gpgpu/static_shared_cuda.cu"
   vercors should verify using silicon example "concepts/gpgpu/static_shared_opencl.cl"
+
+  vercors should verify using silicon example "concepts/gpgpu/global_fence_opencl.cl"
   // https://github.com/utwente-fmt/vercors/issues/852
   // vercors should verify using silicon example "concepts/gpgpu/GPGPU-Example-updates.cu"
   // https://github.com/utwente-fmt/vercors/issues/856
