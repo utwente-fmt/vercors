80be9faab2550960df92c23aec7e2aee02d5b625
Philip Tasche <p.b.h.tasche@utwente.nl>
2024-04-30 15:21:25 +0200

    Simplified implementation of constraint map

--- src/rewrite/vct/rewrite/rasi/ConstraintMap.scala
+++ src/rewrite/vct/rewrite/rasi/ConstraintMap.scala
@@ -1,6 +1,6 @@
 package vct.rewrite.rasi
 
-case class ConstraintMap[G](constraints: Map[ResolvableVariable[G], Set[UncertainValue]]) {
+case class ConstraintMap[G](constraints: Map[ResolvableVariable[G], UncertainValue]) {
   /**
    * Concatenates the constraints from two constraint maps into one. This is semantically equivalent to an "and"
    * operation on the two constraint sets.
@@ -8,13 +8,8 @@ case class ConstraintMap[G](constraints: Map[ResolvableVariable[G], Set[Uncertai
    * @param other Constraint map to concatenate this one with
    * @return A new constraint map representing the conjunction of the arguments
    */
-  def &&(other: ConstraintMap[G]): ConstraintMap[G] = {
-    var map: Map[ResolvableVariable[G], Set[UncertainValue]] = constraints
-    for (e <- other.constraints) {
-      map = map + (e._1 -> (map.getOrElse(e._1, Set()) ++ e._2))
-    }
-    ConstraintMap(map)
-  }
+  def &&(other: ConstraintMap[G]): ConstraintMap[G] =
+    ConstraintMap(constraints ++ other.constraints.map{ case (k, v) => k -> v.intersection(constraints.getOrElse(k, UncertainValue.uncertain_of(v.t[G]))) })
 
   /**
    * Combines the constraints from two constraint maps into one that represents the disjunction of both inputs. The
@@ -24,17 +19,8 @@ case class ConstraintMap[G](constraints: Map[ResolvableVariable[G], Set[Uncertai
    * @param other Constraint map to combine this one with
    * @return A new constraint map representing the disjunction of the arguments
    */
-  def ||(other: ConstraintMap[G]): ConstraintMap[G] = {
-    val this_values: Map[ResolvableVariable[G], UncertainValue] = this.resolve
-    val other_values: Map[ResolvableVariable[G], UncertainValue] = other.resolve
-    var map: Map[ResolvableVariable[G], Set[UncertainValue]] = Map.empty[ResolvableVariable[G], Set[UncertainValue]]
-    for (e <- this_values.keySet) {
-      if (other_values.contains(e)) {
-        map += (e -> Set(this_values(e).union(other_values(e))))
-      }
-    }
-    ConstraintMap(map)
-  }
+  def ||(other: ConstraintMap[G]): ConstraintMap[G] =
+    ConstraintMap.from_cons(constraints.keySet.intersect(other.constraints.keySet).map(k => k -> constraints(k).union(other.constraints(k))))
 
   /**
    * Resolves the various constraints placed on each variable in the map and returns a simpler map mapping every
@@ -42,8 +28,7 @@ case class ConstraintMap[G](constraints: Map[ResolvableVariable[G], Set[Uncertai
    *
    * @return A representation of this map in which each variable is mapped to one uncertain value
    */
-  def resolve: Map[ResolvableVariable[G], UncertainValue] =
-    constraints.map(e => e._1 -> e._2.reduce((v1, v2) => v1.intersection(v2)))
+  def resolve: Map[ResolvableVariable[G], UncertainValue] = constraints
 
   /**
    * Checks whether this map's valuation is impossible. A valuation is impossible if any variable is mapped to an empty
@@ -55,13 +40,13 @@ case class ConstraintMap[G](constraints: Map[ResolvableVariable[G], Set[Uncertai
 }
 case object ConstraintMap {
   def from[G](variable: ResolvableVariable[G], value: UncertainValue): ConstraintMap[G] =
-    ConstraintMap(Map.from(Seq(variable -> Set(value))))
+    ConstraintMap(Map.from(Seq(variable -> value)))
 
   def from_cons[G](cons: Set[(ResolvableVariable[G], UncertainValue)]): ConstraintMap[G] =
-    ConstraintMap(Map.from(cons.map(t => t._1 -> Set(t._2))))
+    ConstraintMap(Map.from(cons.map(t => t._1 -> t._2)))
 
   def empty[G]: ConstraintMap[G] = ConstraintMap(Map.empty)
 
   def impossible[G](vars: Set[_ <: ResolvableVariable[G]]): ConstraintMap[G] =
-    ConstraintMap(Map.from(vars.map(v => v -> Set(UncertainValue.empty_of(v.t)))))
+    ConstraintMap(Map.from(vars.map(v => v -> UncertainValue.empty_of(v.t))))
 }
