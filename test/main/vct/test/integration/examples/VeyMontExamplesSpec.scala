8203a23a4d9e8d211c23fd039190a80fd33cf8c8
Bob Rubbens <bobrubbens@gmail.com>
2024-05-03 15:28:56 +0200

    Repair veymont generated permission examples

--- test/main/vct/test/integration/examples/VeyMontExamplesSpec.scala
+++ test/main/vct/test/integration/examples/VeyMontExamplesSpec.scala
@@ -3,7 +3,7 @@ package vct.test.integration.examples
 import vct.test.integration.helper.VercorsSpec
 
 class VeyMontExamplesSpec extends VercorsSpec {
-  val wd = "concepts/veymont"
+  val wd = "concepts/veymont/generatedPermissions"
   vercors should verify using silicon flags "--veymont-generate-permissions" examples(
     s"$wd/TicTacToe/Player.pvl",
     s"$wd/TicTacToe/Move.pvl",
