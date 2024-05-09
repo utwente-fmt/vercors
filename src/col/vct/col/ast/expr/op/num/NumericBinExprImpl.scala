c7992e29227d228256461f350c829a6a3c4bc72a
Lars <l.b.vandenhaak@gmail.com>
2024-05-09 09:21:13 +0200

    Made some types lazy fixing #1195 partly

--- src/col/vct/col/ast/expr/op/num/NumericBinExprImpl.scala
+++ src/col/vct/col/ast/expr/op/num/NumericBinExprImpl.scala
@@ -4,8 +4,5 @@ import vct.col.ast.{NumericBinExpr, TInt, TRational, Type}
 import vct.col.typerules.CoercionUtils
 
 trait NumericBinExprImpl[G] { this: NumericBinExpr[G] =>
-
-  override def t: Type[G] = {
-    getNumericType
-  }
+  lazy val t: Type[G] = getNumericType
 }
