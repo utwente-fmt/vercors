958b5b72818d7e36af44596fabc71024cd2404ba
Pieter Bos <p.h.bos@utwente.nl>
2024-05-24 17:25:01 +0200

    start parsing grammar

--- src/textMateGenerator/gen/Main.scala
+++ src/textMateGenerator/gen/Main.scala
@@ -3,14 +3,17 @@ package gen
 import hre.io.{RWFile, Readable}
 import upickle.default.{macroRW, ReadWriter => RW}
 import upickle._
+
 import java.nio.file.Paths
 import org.antlr.v4.runtime
+import org.antlr.v4.runtime.misc.IntervalSet
 import org.antlr.v4.runtime.{CharStreams, CommonTokenStream}
-import vct.antlr4.generated.ANTLRv4Parser.Rules0Context
-import vct.antlr4.generated.ANTLRv4ParserPatterns.{GrammarSpec0, RuleSpec0, Rules0}
+import vct.antlr4.generated.ANTLRv4Parser.{LexerAltContext, LexerRuleSpecContext, Rules0Context}
+import vct.antlr4.generated.ANTLRv4ParserPatterns._
 import vct.antlr4.generated.{ANTLRv4Lexer, ANTLRv4Parser}
 
 import java.io.FileNotFoundException
+import scala.jdk.CollectionConverters.CollectionHasAsScala
 
 case object Main {
 
@@ -32,19 +35,29 @@ case object Main {
   def main(args: Array[String]): Unit = {
     var textMateGrammar = CGL(args(0), args(1), Nil)
     val tree = parse(args(2))
-    tree match {
-      case GrammarSpec0(grammarDecl, prequelConstructs, rules, modeSpec, _) =>
-        rules match {
-          case Rules0(specRules) =>
-            for (ruleText <- specRules.map(rule => rule.getText))
-              if (ruleText.matches(".*\\{.*\\}.*"))
-                textMateGrammar = textMateGrammar.addPattern(
-                  MatchPattern(ruleText.substring(ruleText.indexOf('{') + 2, ruleText.indexOf("}") - 2),
-                    escapeAndEncloseWords(ruleText.split('\'')(1)))
-                )
-             }
-    }
-    print(upickle.default.write(textMateGrammar, indent = 2))
+    val GrammarSpec0(
+      GrammarDecl0(_, grammarName, _),
+      _,
+      Rules0(modelessRules),
+      modedRules,
+      _
+    ) = tree
+
+    val rules: Seq[LexerRuleSpecContext] =
+      modelessRules.map { case RuleSpec0(rule) => rule } ++
+        modedRules.flatMap { case ModeSpec0(_, _, _, rules) => rules }
+
+    rules.foreach(processRule)
+
+    // print(upickle.default.write(textMateGrammar, indent = 2))
+  }
+
+  def processRule(rule: LexerRuleSpecContext): Unit = {
+    val LexerRuleSpec0(_, name, _, _, LexerRuleBlock0(altsRule), _) = rule
+    val alts = altsRule.children.asScala.collect { case alt: LexerAltContext => alt }
+    val set = new IntervalSet()
+    set.addAll(set)
+    println(name)
   }
 
   def escapeAndEncloseWords(text: String): String = {
