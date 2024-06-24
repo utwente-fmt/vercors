33e12d6222070a5d3cebd8c9d723f0a593767801
Philip Tasche <p.b.h.tasche@utwente.nl>
2024-06-24 14:24:59 +0200

    Remove printing of trivial scales for predicate applications

--- src/col/vct/col/ast/expr/apply/InstancePredicateApplyImpl.scala
+++ src/col/vct/col/ast/expr/apply/InstancePredicateApplyImpl.scala
@@ -1,6 +1,6 @@
 package vct.col.ast.expr.apply
 
-import vct.col.ast.InstancePredicateApply
+import vct.col.ast.{InstancePredicateApply, WritePerm}
 import vct.col.print.{Ctx, Doc, Group, Precedence, Text}
 import vct.col.ast.ops.InstancePredicateApplyOps
 
@@ -9,7 +9,11 @@ trait InstancePredicateApplyImpl[G] extends InstancePredicateApplyOps[G] {
   override def precedence: Int = Precedence.PREFIX
   override def layout(implicit ctx: Ctx): Doc =
     Group(
-      Text("[") <> perm <> "]" <> obj.bind(Precedence.POSTFIX) <> "." <>
-        ctx.name(ref) <> "(" <> Doc.args(args) <> ")"
+      if (!perm.isInstanceOf[WritePerm[G]])
+        Text("[") <> perm <> "]" <> obj.bind(Precedence.POSTFIX) <> "." <>
+          ctx.name(ref) <> "(" <> Doc.args(args) <> ")"
+      else
+        Text("") <> obj.bind(Precedence.POSTFIX) <> "." <> ctx.name(ref) <>
+          "(" <> Doc.args(args) <> ")"
     )
 }
--- src/col/vct/col/ast/expr/apply/PredicateApplyImpl.scala
+++ src/col/vct/col/ast/expr/apply/PredicateApplyImpl.scala
@@ -1,6 +1,6 @@
 package vct.col.ast.expr.apply
 
-import vct.col.ast.PredicateApply
+import vct.col.ast.{PredicateApply, WritePerm}
 import vct.col.print.{Ctx, Doc, DocUtil, Empty, Group, Precedence, Text}
 import vct.col.ast.ops.PredicateApplyOps
 
@@ -16,7 +16,11 @@ trait PredicateApplyImpl[G] extends PredicateApplyOps[G] {
 
   def layoutSpec(implicit ctx: Ctx): Doc =
     Group(
-      Text("[") <> perm <> "]" <> ctx.name(ref) <> "(" <> Doc.args(args) <> ")"
+      if (!perm.isInstanceOf[WritePerm[G]])
+        Text("[") <> perm <> "]" <> ctx.name(ref) <> "(" <> Doc.args(args) <>
+          ")"
+      else
+        Text(ctx.name(ref)) <> "(" <> Doc.args(args) <> ")"
     )
 
   override def layout(implicit ctx: Ctx): Doc =
