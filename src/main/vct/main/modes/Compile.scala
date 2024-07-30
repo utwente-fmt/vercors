86286fe3a535103c603aee97611cc3ed233596a9
Bob Rubbens <bobrubbens@gmail.com>
2024-07-30 11:48:30 +0200

    Factor out PVL to Java compilation.
    - Make pvl-to-java compatibility transformation in the veymont pipeline
      depend on whether pvl or java is being generated
    - Fix small bug in NewArrayImpl where plain toString was used instead of
      Doc. This caused an unpretty name to be used, instead of the name
      indicated by ctx.
    - Factor out the pvl-generic part of veymont implementation generation
      and make it available under the --compile flag.

---
+++ src/main/vct/main/modes/Compile.scala
@@ -0,0 +1,31 @@
+package vct.main.modes
+
+import com.typesafe.scalalogging.LazyLogging
+import vct.col.origin.BlameCollector
+import vct.col.print.Ctx
+import vct.col.rewrite.bip.BIP
+import vct.main.Main
+import vct.main.stages.{Output, Parsing, Resolution, Transformation}
+import vct.options.Options
+import vct.parsers.transform.ConstantBlameProvider
+
+import java.nio.file.Paths
+
+case object Compile extends LazyLogging {
+  def runOptions(options: Options): Int = {
+    if (options.inputs.isEmpty) {
+      logger.warn("No inputs found, not compiling anything")
+    }
+
+    val collector = BlameCollector()
+    val blameProvider = ConstantBlameProvider(collector)
+    Parsing.ofOptions(options, blameProvider)
+      .thenRun(Resolution.ofOptions(options, blameProvider))
+      .thenRun(Transformation.pvlJavaCompatOfOptions(options)).thenRun(Output(
+        options.compileOutput.orElse(Some(Paths.get("a.java"))),
+        Ctx.Java,
+        false,
+      )).run(options.inputs)
+    Main.EXIT_CODE_SUCCESS
+  }
+}
