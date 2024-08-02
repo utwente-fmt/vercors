4735b58c459ed9b882e40186888ea7f7d18ce5fc
Bob Rubbens <bobrubbens@gmail.com>
2024-08-02 16:41:34 +0200

    HAlfway through refactoring the veymont frontend. Now need to re-implement some more flags.

--- src/main/vct/main/modes/VeyMont.scala
+++ src/main/vct/main/modes/VeyMont.scala
@@ -2,15 +2,21 @@ package vct.main.modes
 
 import com.typesafe.scalalogging.LazyLogging
 import hre.io.{CollectString, LiteralReadable, Readable}
+import hre.progress.Progress
 import hre.stages.{IdentityStage, Stage, Stages}
 import hre.stages.Stages.{applyIf, branch, saveInput, timed}
 import hre.util.Time.logTime
 import vct.col.ast.Verification
-import vct.col.origin.{BlameCollector, VerificationFailure}
+import vct.col.origin.{BlameCollector, TableEntry, VerificationFailure}
 import vct.col.print.Ctx
 import vct.col.rewrite.Generation
 import vct.col.rewrite.bip.BIP
-import vct.main.Main.{EXIT_CODE_ERROR, EXIT_CODE_SUCCESS}
+import vct.main.Main.{
+  EXIT_CODE_ERROR,
+  EXIT_CODE_SUCCESS,
+  EXIT_CODE_VERIFICATION_FAILURE,
+}
+import vct.main.modes.Verify.logger
 import vct.main.stages.{
   Backend,
   ExpectedErrors,
@@ -22,17 +28,29 @@ import vct.main.stages.{
 import vct.options.Options
 import vct.parsers.transform.ConstantBlameProvider
 import vct.result.VerificationError
-import vct.result.VerificationError.UserError
+import vct.result.VerificationError.{SystemError, UserError}
 
 object VeyMont extends LazyLogging {
-  case class ChoreographyVerificationError(failures: Seq[VerificationFailure])
-      extends UserError {
-    override def text: String = {
-      val fails = failures.map(_.desc).mkString("\n")
-      s"Verification of the input choreography failed because of the following failures:\n$fails"
-    }
-
-    override def code: String = "veymont:choreographyVerificationFailed"
+  case object WrapVerificationError {
+    def wrap(codePrefix: String, textPrefix: String)(
+        error: VerificationError
+    ): VerificationError =
+      error match {
+        case error: UserError => WrapUserError(codePrefix, textPrefix, error)
+        case error: SystemError => WrapSystemError(textPrefix, error)
+      }
+  }
+  case class WrapUserError(
+      codePrefix: String,
+      textPrefix: String,
+      error: UserError,
+  ) extends UserError {
+    override def code: String = s"$codePrefix:${error.code}"
+    override def text: String = s"$textPrefix\n${error.text}"
+  }
+  case class WrapSystemError(textPrefix: String, error: SystemError)
+      extends SystemError {
+    override def text: String = s"$textPrefix\n${error.text}"
   }
 
   case class ImplementationVerificationError(failures: Seq[VerificationFailure])
@@ -53,6 +71,153 @@ object VeyMont extends LazyLogging {
     override def progressWeight: Int = 1
     override def run(in: Unit): Unit =
       if (collector.errs.nonEmpty) { throw error(collector.errs.toSeq) }
+  }
+
+  def choreographyWithOptions(
+      options: Options,
+      inputs: Seq[Readable],
+  ): Either[VerificationError, ChoreographyResult] = {
+    Progress.stages(Seq(("Parsing", 2), ("Verification", 15))) { next =>
+      val collector = BlameCollector()
+      val blameProvider = ConstantBlameProvider(collector)
+      // TODO (RR): delete veymontGeneratePermissions to have just one
+      val parsingStages = Parsing.ofOptions(options, blameProvider)
+        .thenRun(Resolution.ofOptions(options, blameProvider))
+
+      val wrap: VerificationError => VerificationError = WrapVerificationError
+        .wrap(
+          "choreography",
+          "The following error occurred during choreography verification:",
+        )
+
+      val program =
+        parsingStages.run(inputs) match {
+          case Left(err) => return Left(wrap(err))
+          case Right(program) => program
+        }
+
+      next()
+
+      val verificationStages = Transformation
+        .ofOptions(options, BIP.VerificationResults())
+        .thenRun(Backend.ofOptions(options))
+        .thenRun(ExpectedErrors.ofOptions(options))
+
+      verificationStages.run(program) match {
+        case Left(error) => Left(wrap(error))
+        case Right(()) =>
+          Right(ChoreographyResult(program, collector.errs.toSeq))
+      }
+
+      // TODO (RR): Reinstate this log?
+//          logger.info("Choreographic verification successful ✔\uFE0F")
+//              ),
+//            ))).transform(_._1)
+//          }
+    }
+  }
+
+  def generateWithOptions(
+      options: Options,
+      program: Verification[_ <: Generation],
+  ): Either[VerificationError, GenerateResult] = {
+    val generateJava = options.veymontOutput
+      .exists(_.toString.endsWith(".java"))
+    val generateStages = Transformation
+      .veymontImplementationGenerationOfOptions(options)
+      .thenRun(applyIf[Verification[_ <: Generation]](
+        generateJava,
+        IdentityStage().thenRun(Transformation.pvlJavaCompatOfOptions(options)),
+      )).thenRun(Output(
+        options.veymontOutput,
+        if (generateJava)
+          Ctx.Java
+        else
+          Ctx.PVL,
+        false,
+      ))
+    generateStages.run(program) match {
+      case Left(err) =>
+        Left(
+          WrapVerificationError.wrap(
+            "generate",
+            "The following error occurred while generating an implementation:",
+          )(err)
+        )
+      case Right(Seq(lit)) => Right(GenerateResult(lit))
+      case Right(_) => ??? // Should always be only 1 literal readable
+    }
+  }
+
+  def implementationWithOptions(
+      options: Options,
+      implementation: LiteralReadable,
+  ): Either[VerificationError, ImplementationResult] = {
+    val collector = BlameCollector()
+    val bipResults = BIP.VerificationResults()
+    val blameProvider = ConstantBlameProvider(collector)
+    // TODO: Delete?
+//    val options = globalOptions.copy(generatePermissions = false)
+
+    val stages = vct.main.stages.Stages
+      .ofOptions(options, blameProvider, bipResults)
+      .thenRun(VeyMont.NoVerificationFailures(
+        collector,
+        VeyMont.ImplementationVerificationError,
+      ))
+
+    stages.run(Seq(implementation)) match {
+      case Left(err) =>
+        Left(
+          WrapVerificationError.wrap(
+            "implementation",
+            "The following error occurred during verification of the implementation:",
+          )(err)
+        )
+      case Right(()) => Right(ImplementationResult(collector.errs.toSeq))
+    }
+  }
+
+  sealed trait StageResult
+  case class ChoreographyResult(
+      program: Verification[_ <: Generation],
+      failures: Seq[VerificationFailure],
+  ) extends StageResult
+  case class GenerateResult(implementation: LiteralReadable) extends StageResult
+  case class ImplementationResult(failures: Seq[VerificationFailure])
+      extends StageResult
+
+  def ofOptions(options: Options): Either[VerificationError, StageResult] = {
+    Progress.stages(Seq(("VeyMont", 1))) { _ =>
+      Progress.stages(
+        Seq(("Choreography", 10), ("Generate", 2), ("Implementation", 9))
+      ) { next =>
+        // TODO: record choreographyphase in error
+        val program =
+          choreographyWithOptions(options, options.inputs) match {
+            case Left(err) => return Left(err)
+            case res @ Right(ChoreographyResult(_, failures))
+                if failures.nonEmpty =>
+              return res
+            case Right(ChoreographyResult(program, Seq())) => program
+          }
+
+        logger.info("Choreographic verification success")
+
+        next()
+
+        val implementation =
+          generateWithOptions(options, program)
+            .fold[GenerateResult](err => return Left(err), lit => lit)
+            .implementation
+
+        logger.info("Implementation generation success")
+
+        next()
+
+        implementationWithOptions(options, implementation)
+      }
+    }
   }
 
   def stagesOfOptions(globalOptions: Options): Stages[Seq[Readable], Unit] = {
@@ -82,7 +247,8 @@ object VeyMont extends LazyLogging {
               .thenRun(ExpectedErrors.ofOptions(options))
               .thenRun(VeyMont.NoVerificationFailures(
                 collector,
-                VeyMont.ChoreographyVerificationError,
+//                VeyMont.ChoreographyVerificationError,
+                ???,
               )).also(logger.info(
                 "Choreographic verification successful ✔\uFE0F"
               )),
@@ -141,25 +307,62 @@ object VeyMont extends LazyLogging {
       .thenRun(implementationVerificationStage)
   }
 
-  def verifyGenerateOptions(options: Options): Either[VerificationError, Unit] =
+  def verifyGenerateOptions(
+      options: Options
+  ): Either[VerificationError, Unit] = {
+    // TODO (RR): Skipping, focusing, etc.
     logTime(
       "VeyMont mode", {
-        val stages = stagesOfOptions(options)
-        stages.run(options.inputs)
+        Progress.stages(Seq(("VeyMont", 1))) { _ =>
+          Progress.stages(Seq(
+            ("Choreography", 10),
+            ("Generation", 2),
+            ("Implementation verification", 9),
+          )) { next =>
+            val stages = stagesOfOptions(options)
+            stages.run(options.inputs)
+          }
+        }
       },
     )
+  }
 
   def runOptions(options: Options): Int =
-    verifyGenerateOptions(options) match {
+    ofOptions(options) match {
       case Left(err: VerificationError.UserError) =>
         logger.error(err.text)
         EXIT_CODE_ERROR
       case Left(err: VerificationError.SystemError) =>
         logger.error(CollectString(s => err.printStackTrace(s)))
         EXIT_CODE_ERROR
-      case Right(()) =>
-        logger.info("VeyMont success")
-        EXIT_CODE_SUCCESS
+      case Right(result) =>
+        result match {
+          case ChoreographyResult(_, fails) if fails.nonEmpty =>
+            logger.error("Verification of choreography failed")
+            if (options.more || fails.size <= 2)
+              fails.foreach(fail => logger.error(fail.desc))
+            else {
+              logger.info(
+                "Printing verification results as a compressed table. Run with `--more` for verbose verification results."
+              )
+              logger.error(TableEntry.render(fails.map(_.asTableEntry)))
+            }
+            EXIT_CODE_VERIFICATION_FAILURE
+          case ChoreographyResult(_, _) => EXIT_CODE_SUCCESS
+          case GenerateResult(_) => EXIT_CODE_SUCCESS
+          case ImplementationResult(fails) if fails.nonEmpty =>
+            logger.error("Verification of generated implementation failed")
+            if (options.more || fails.size <= 2)
+              fails.foreach(fail => logger.error(fail.desc))
+            else {
+              logger.info(
+                "Printing verification results as a compressed table. Run with `--more` for verbose verification results."
+              )
+              logger.error(TableEntry.render(fails.map(_.asTableEntry)))
+            }
+            EXIT_CODE_VERIFICATION_FAILURE
+          case ImplementationResult(_) => EXIT_CODE_SUCCESS
+        }
     }
 
 }
