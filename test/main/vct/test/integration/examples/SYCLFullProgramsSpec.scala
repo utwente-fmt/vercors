767f02c2eb308c64e1d4081542e14e9753d083e3
Alexander Stekelenburg <a.v.stekelenburg@utwente.nl>
2024-09-12 15:45:20 +0200

    Try out resource bounds for failing SYCL example

--- test/main/vct/test/integration/examples/SYCLFullProgramsSpec.scala
+++ test/main/vct/test/integration/examples/SYCLFullProgramsSpec.scala
@@ -6,5 +6,5 @@ class SYCLFullProgramsSpec extends VercorsSpec {
   vercors should verify using silicon flag "--no-infer-heap-context-into-frame" example "concepts/sycl/fullExamples/VectorAdd.cpp"
 
   // About 1 in 5 times this test will fail, because then VerCors fails to prove the pre-conditions of the second kernel even though they are true
-  vercors should verify using silicon flag "--no-infer-heap-context-into-frame" example "concepts/sycl/fullExamples/MatrixTransposeWithF.cpp"
+  vercors should verify using silicon flags("--no-infer-heap-context-into-frame", "--backend-option", "--proverEnableResourceBounds") example "concepts/sycl/fullExamples/MatrixTransposeWithF.cpp"
 }
