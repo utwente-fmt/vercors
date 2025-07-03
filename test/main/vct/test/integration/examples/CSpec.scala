ed6bc9a3fa18a03fb3b065565e17a532e3e6e2c0
Alexander Stekelenburg <a.v.stekelenburg@utwente.nl>
2025-07-03 17:55:01 +0200

    Add --dev-no-sat to test flags

--- test/main/vct/test/integration/examples/CSpec.scala
+++ test/main/vct/test/integration/examples/CSpec.scala
@@ -14,7 +14,7 @@ class CSpec extends VercorsSpec {
   vercors should verify using silicon example "concepts/c/pointer_casts.c"
   vercors should verify using silicon example "concepts/c/pointer_tests.c"
   vercors should verify using silicon example "concepts/c/pointer_relations.c"
-  vercors should verify using silicon flag "prover-config:smt.arith.solver=6" example "concepts/c/pointer_array.c"
+  vercors should verify using silicon flags("--dev-no-sat", "--prover-config:smt.arith.solver=6") example "concepts/c/pointer_array.c"
   vercors should verify using silicon flags("--backend-option", "--exhaleMode=2") example "concepts/c/tagged_struct.c"
   vercors should verify using silicon example "concepts/c/void.c"
 
