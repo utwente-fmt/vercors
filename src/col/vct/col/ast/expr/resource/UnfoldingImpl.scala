1545773207681f81885138d470bc344ae85705a3
ArmborstL <l.armborst@utwente.nl>
2026-02-17 18:12:52 +0100

    more generic fix for Issue #1403

--- src/col/vct/col/ast/expr/resource/UnfoldingImpl.scala
+++ src/col/vct/col/ast/expr/resource/UnfoldingImpl.scala
@@ -1,13 +1,7 @@
 package vct.col.ast.expr.resource
 
 import vct.col.ast.node.NodeFamilyImpl
-import vct.col.ast.{
-  AmbiguousFoldTarget,
-  Node,
-  ScaledPredicateApply,
-  Type,
-  Unfolding,
-}
+import vct.col.ast.{Node, Type, Unfolding}
 import vct.col.print.{Ctx, Doc, Group, Precedence, Text}
 import vct.col.ast.ops.UnfoldingOps
 import vct.col.check.{CheckContext, ResourceInPostcondition}
@@ -44,22 +38,21 @@ trait UnfoldingImpl[G] extends NodeFamilyImpl[G] with UnfoldingOps[G] {
     /* in postconditions of pure functions, ResourceTerms are forbidden and throw an error.
        However, "\unfolding res() \in expr" should be allowed, so we filter out the error if the offending
        ResourceTerm is the target of this Unfolding expression.
-       ToDo: This is a quick-fix, checking that the offending term is exactly the target; it does not work if the
-        target is e.g. a scale expression. For a more generic solution, we might need e.g. a flag in the context to
-        indicate "we're in an unfolding target, ResourceTerms are allowed now"
      */
-    val res = super.checkContextRecursor(context, f)
+    // modified version of super.checkContextRecursor
+    val res = subnodes.map(n => (n, f(enterCheckContext(context), n)))
+    // filtering
     res.map {
-      case s: Seq[_] =>
-        s.filter {
-          case ResourceInPostcondition(n) =>
-            this.res match {
-              case AmbiguousFoldTarget(t) if t == n => false
+      case (this.res, errors) =>
+        errors match {
+          case s: Seq[_] =>
+            s.filter {
+              case ResourceInPostcondition(_) => false
               case _ => true
-            }
-          case _ => true
-        }.asInstanceOf[T]
-      case other => other
+            }.asInstanceOf[T]
+          case other => other
+        }
+      case (_, errors) => errors
     }
   }
 }
