bd0316954f3e1f444c30c836b33e53597338c92f
Ömer Şakar <o.f.o.sakar@utwente.nl>
2026-02-17 12:32:56 +0100

    Added arithmetic flag to testcase

--- test/main/vct/test/integration/examples/ArrayExamplesSpec.scala
+++ test/main/vct/test/integration/examples/ArrayExamplesSpec.scala
@@ -10,7 +10,6 @@ class ArrayExamplesSpec extends VercorsSpec {
   vercors should verify using silicon example "concepts/arrays/fibSearch.java"
 
   // https://github.com/utwente-fmt/vercors/issues/791
-  // vercors should verify using silicon example "concepts/arrays/Transpose.pvl"
   // vercors should verify using silicon example "concepts/arrays/access-sub-matrix-fail.c"
   // vercors should verify using silicon example "concepts/arrays/access-sub-matrix-pass.c"
 //   vercors should verify using silicon example "concepts/arrays/zero-sub-matrix.c"
@@ -25,6 +24,6 @@ class ArrayExamplesSpec extends VercorsSpec {
 
   vercors should verify using silicon example "concepts/arrays/ArrayList.java"
 
-  vercors should verify using silicon example "concepts/arrays/MatrixTranspose.pvl"
+  vercors should verify using silicon flags("--prover-config:smt.arith.solver=6") example "concepts/arrays/MatrixTranspose.pvl"
 
 }
