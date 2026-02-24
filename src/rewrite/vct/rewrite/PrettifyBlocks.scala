8036beacce7813aebc46980483b705e61abcb451
Alexander Stekelenburg <a.v.stekelenburg@utwente.nl>
2026-02-24 13:37:38 +0100

    Change PrettifyBlocks such that it does not flatten scopes (fixes #1301, supercedes #1375)

--- src/rewrite/vct/rewrite/PrettifyBlocks.scala
+++ src/rewrite/vct/rewrite/PrettifyBlocks.scala
@@ -1,12 +1,7 @@
-package vct.col.rewrite
+package vct.rewrite
 
-import hre.util.ScopedStack
 import vct.col.ast._
-import vct.col.rewrite.{Generation, Rewriter, Rewritten}
-import RewriteHelpers._
-import vct.col.util.AstBuildHelpers._
-
-import scala.collection.mutable.ArrayBuffer
+import vct.col.rewrite.{Generation, Rewriter, RewriterBuilder}
 
 object PrettifyBlocks extends RewriterBuilder {
   override def key: String = "prettifyBlocks"
@@ -15,25 +10,10 @@ object PrettifyBlocks extends RewriterBuilder {
 }
 
 case class PrettifyBlocks[Pre <: Generation]() extends Rewriter[Pre] {
-  def collectVariables(
-      body: Statement[Pre],
-      extra: Seq[Variable[Pre]] = Nil,
-  ): Statement[Post] = {
-    val (variablesHere, newBody) = variables.collect {
-      extra.foreach(dispatch)
-      dispatch(body)
-    }
-
-    Scope(variablesHere, newBody)(body.o)
-  }
-
-  def dispatchFlatly(stat: Statement[Pre]): Seq[Statement[Post]] =
+  private def dispatchFlatly(stat: Statement[Pre]): Seq[Statement[Post]] =
     stat match {
       case Block(statements) => statements.flatMap(dispatchFlatly)
-      case Scope(Nil, body) => dispatchFlatly(body)
-      case Scope(locals, body) if variables.nonEmpty =>
-        locals.foreach(dispatch)
-        dispatchFlatly(body)
+      case Scope(Nil, Block(Nil)) => Nil
       case other => Seq(dispatch(other))
     }
 
@@ -41,25 +21,14 @@ case class PrettifyBlocks[Pre <: Generation]() extends Rewriter[Pre] {
     stat match {
       case Block(statements) =>
         Block(statements.flatMap(dispatchFlatly))(stat.o)
-      case Scope(Nil, body) => dispatch(body)
+      case Scope(Nil, Block(Nil)) => Block[Post](Nil)(stat.o)
       case Scope(locals, body) =>
-        if (variables.nonEmpty) {
+        val (variablesHere, newBody) = variables.collect {
           locals.foreach(dispatch)
           dispatch(body)
-        } else { collectVariables(body, extra = locals) }
-
-      case loop @ Loop(_, _, _, _: IterationContract[Pre], body) =>
-        loop.rewrite(body = collectVariables(body))
-
-      case vec: VecBlock[Pre] =>
-        vec.rewrite(content = collectVariables(vec.content))
-
-      case act: ModelDo[Pre] => act.rewrite(impl = collectVariables(act.impl))
-
-      case pack: WandPackage[Pre] =>
-        pack.rewrite(proof = collectVariables(pack.proof))
-
-      case other => rewriteDefault(other)
+        }
+        Scope(variablesHere, newBody)(body.o)
+      case _ => super.dispatch(stat)
     }
 
   override def dispatch(e: Expr[Pre]): Expr[Post] =
@@ -67,30 +36,6 @@ case class PrettifyBlocks[Pre <: Generation]() extends Rewriter[Pre] {
       case ScopedExpr(locals, body) =>
         locals.foreach(dispatch)
         dispatch(body)
-      case other => rewriteDefault(other)
-    }
-
-  override def dispatch(parRegion: ParRegion[Pre]): ParRegion[Rewritten[Pre]] =
-    parRegion match {
-      case block: ParBlock[Pre] =>
-        block.rewrite(content = collectVariables(block.content))
-      case other => rewriteDefault(other)
-    }
-
-  override def dispatch(decl: Declaration[Pre]): Unit =
-    decl match {
-      case method: RunMethod[Pre] =>
-        classDeclarations.succeed(
-          method,
-          method.rewrite(body = method.body.map(collectVariables(_))),
-        )
-      case method: AbstractMethod[Pre] =>
-        allScopes.anySucceedOnly(
-          method,
-          allScopes.anyDeclare(
-            method.rewrite(body = method.body.map(collectVariables(_)))
-          ),
-        )
-      case other => rewriteDefault(other)
+      case _ => super.dispatch(e)
     }
 }
