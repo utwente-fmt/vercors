aa7bf739d7c191bf1e7a0ff4b23da645697b4fd9
mrskr <o.f.o.sakar@utwente.nl>
2026-05-01 20:13:55 +0200

    ShiftGrupSum.cpp also rewritten for warpsize between 8 and 64

--- examples/concepts/sycl/kernels/ShiftGroupSum.cpp
+++ examples/concepts/sycl/kernels/ShiftGroupSum.cpp
@@ -19,7 +19,7 @@
     yields int resultxx;
     yields int resultyy;
     yields int resultzz;
-    context_everywhere T == Tf() && N == Nf() && T > N && T%N == 0 && N%32==0 && N > 0;
+    context_everywhere T == Tf() && N == Nf() && T > N && T%N == 0 && N%sycl::h::warp_sizes()==0 && N > 0;
     context \pointer(fx, T, write) ** \pointer(fy, T, write) ** \pointer(fz, T, write);
     requires |fxGs| == Tf() && (\forall int i=0 .. Tf(); fxGs[i] == fx[i]);
     requires |fyGs| == Tf() && (\forall int i=0 .. Tf(); fyGs[i] == fy[i]);
@@ -35,13 +35,13 @@
 @*/
 void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
     //@ label bK;
-    //@ assert true;
+    //@ refute false;
     {
         sycl::buffer<int, 1> fxBuf = sycl::buffer(fx, sycl::range<1>(T));
         sycl::buffer<int, 1> fyBuf = sycl::buffer(fy, sycl::range<1>(T));
         sycl::buffer<int, 1> fzBuf = sycl::buffer(fz, sycl::range<1>(T));
 
-        //@ assert true;
+        //@ refute false;
 
         sycl::event e0 = q.submit([&](sycl::handler& h)
         {
@@ -69,8 +69,10 @@ void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
                     it.get_sub_group().get_local_id()+it.get_sub_group().get_local_range(0)-2 <= it.get_sub_group().get_local_range(0) ==>
                     it.get_global_id(0)+it.get_sub_group().get_local_range(0)-2 <= |fzGs| ==>
                         fxAcc[it.get_global_id(0)] == sum(fzGs[it.get_global_id(0)-2 .. it.get_global_id(0)+it.get_sub_group().get_local_range(0)-2])); */
-            [=](sycl::nd_item<1> it) [[sycl::reqd_sub_group_size(32)]] {
-                    //@ assert true;
+            [=](sycl::nd_item<1> it) {
+            // [=](sycl::nd_item<1> it) [[sycl::reqd_sub_group_size(sycl::h::warp_sizes())]] {
+
+                    //@ refute false;
                     sycl::sub_group sg = it.get_sub_group();
                     int gid = it.get_global_id(0);
                     int laneId = sg.get_local_id();
@@ -88,7 +90,7 @@ void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
                     /*@ assert lemmaSumOverConcat(fxGs[gid    .. gid+d1],  fxGs[gid+d1 .. gid+d1+d1]) && lemmaSumOverABBCisAC(fxGs, gid,    gid+d1,   gid+d1, gid+d1+d1);
                         assert lemmaSumOverConcat(fzGs[gid    .. gid+d1],  fzGs[gid+d1 .. gid+d1+d1]) && lemmaSumOverABBCisAC(fzGs, gid,    gid+d1,   gid+d1, gid+d1+d1);
                         assert lemmaSumOverConcat(fyGs[gid-d1 .. gid-d1+1],fyGs[gid    .. gid+1])     && lemmaSumOverABBCisAC(fyGs, gid-d1, gid-d1+1, gid, gid+1);
-                        assert true; */
+                        refute false; */
 
                     if (gid % 2 != 0) { fxAcc[gid] = fyAcc[gid]; }
 
@@ -104,7 +106,7 @@ void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
                     /*@ assert lemmaSumOverConcat(fxGs[gid    .. gid+d2],   fxGs[gid+d2 .. gid+d2+d2])  && lemmaSumOverABBCisAC(fxGs, gid,    gid+d2, gid+d2, gid+d2+d2);
                         assert lemmaSumOverConcat(fyGs[gid-d1 .. gid+1],    fyGs[gid+1  .. gid+d2+1])   && lemmaSumOverABBCisAC(fyGs, gid-d1, gid+1,  gid+d2-d1, gid+d2+1);
                         assert lemmaSumOverConcat(fzGs[gid-d2 .. gid-d2+d2],fzGs[gid    .. gid+d2])     && lemmaSumOverABBCisAC(fzGs, gid-d2, gid,    gid, gid+d2);
-                        assert true; */
+                        refute false; */
 
                     if (gid % 4 >= 2) { fxAcc[gid] = fzAcc[gid]; }
 
@@ -112,7 +114,8 @@ void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
                 int dk = 4;
                     //@ ghost int k1 = 2;
 
-                    /*@ loop_invariant k1 >= 2 && dk == sycl::h::exp(2,k1);
+                    /*@ loop_invariant wrpsz == sycl::h::warp_sizes();
+                    loop_invariant k1 >= 2 && dk == sycl::h::exp(2,k1);
                     loop_invariant wrpsz % 2==0;
                     loop_invariant 4 <= dk && dk <= wrpsz && k1 <= logTwo(wrpsz);
                     loop_invariant dk == wrpsz ==> sycl::h::exp(2,k1) == wrpsz;
@@ -139,15 +142,15 @@ void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
                             /*@ assert lemmaSumOverConcat(fxGs[gid   .. gid+dk  ],fxGs[gid+dk   .. gid+dk+dk  ]) && lemmaSumOverABBCisAC(fxGs, gid  , gid+dk  , gid+dk  , gid+dk+dk  );
                                 assert lemmaSumOverConcat(fyGs[gid-1 .. gid+dk-1],fyGs[gid+dk-1 .. gid+dk+dk-1]) && lemmaSumOverABBCisAC(fyGs, gid-1, gid+dk-1, gid+dk-1, gid+dk+dk-1);
                                 assert lemmaSumOverConcat(fzGs[gid-2 .. gid+dk-2],fzGs[gid+dk-2 .. gid+dk+dk-2]) && lemmaSumOverABBCisAC(fzGs, gid-2, gid+dk-2, gid+dk-2, gid+dk+dk-2);
-                                assert true;*/
+                                refute false;*/
 
                             /*@ ghost k1=k1+1; */
                     }
 
             });});
-        //@ assert true;
+        //@ refute false;
         e0.wait();
-        //@ assert true;
+        //@ refute false;
     }
 
     accumulateResult(T,N,fx) 
@@ -163,11 +166,11 @@ void smartsum(sycl::queue q, int T, int N, int* fx, int* fy, int* fz) {
     yields int resultxx;
     yields int resultyy;
     yields int resultzz;
-    context_everywhere T == Tf() && N == Nf() && T > N && T%N == 0 && N%32==0 && N > 0;
+    context_everywhere T == Tf() && N == Nf() && T > N && T%N == 0 && N%sycl::h::warp_sizes()==0 && N > 0;
     context_everywhere \pointer(fx, T, 1\2);
-    context_everywhere (\forall int lid1=0 .. N, int gid1=0 .. T/N; ({:1:sycl::linearize2(gid1, lid1, T/N, N):}%4 == 0 ==>                      (lid1%32+32 <= 32 ==>sycl::linearize2(gid1, lid1, T/N, N)+32 <= |fxGs| ==>fx[sycl::linearize2(gid1, lid1, T/N, N)] == sum(fxGs[sycl::linearize2(gid1, lid1, T/N, N)   .. sycl::linearize2(gid1, lid1, T/N, N)+32]))));
-    context_everywhere (\forall int lid1=0 .. N, int gid1=0 .. T/N; ({:2:sycl::linearize2(gid1, lid1, T/N, N):}%4 == 1 ==> (0 <= lid1%32 - 1 ==> lid1%32+32-1 <= 32 ==> sycl::linearize2(gid1, lid1, T/N, N)+32-1 <= |fyGs| ==>                             fx[sycl::linearize2(gid1, lid1, T/N, N)] == sum(fyGs[sycl::linearize2(gid1, lid1, T/N, N)-1 .. sycl::linearize2(gid1, lid1, T/N, N)+32-1]))));
-    context_everywhere (\forall int lid1=0 .. N, int gid1=0 .. T/N; ({:3:sycl::linearize2(gid1, lid1, T/N, N):}%4 == 2 ==> (0 <= lid1%32 - 2 ==> lid1%32+32-2 <= 32 ==> sycl::linearize2(gid1, lid1, T/N, N)+32-2 <= |fzGs| ==> fx[sycl::linearize2(gid1, lid1, T/N, N)] == sum(fzGs[sycl::linearize2(gid1, lid1, T/N, N)-2 .. sycl::linearize2(gid1, lid1, T/N, N)+32-2]))));
+    context_everywhere (\forall int lid1=0 .. N, int gid1=0 .. T/N; ({:1:sycl::linearize2(gid1, lid1, T/N, N):}%4 == 0 ==>                      (lid1%sycl::h::warp_sizes()+sycl::h::warp_sizes() <= sycl::h::warp_sizes() ==>sycl::linearize2(gid1, lid1, T/N, N)+sycl::h::warp_sizes() <= |fxGs| ==>fx[sycl::linearize2(gid1, lid1, T/N, N)] == sum(fxGs[sycl::linearize2(gid1, lid1, T/N, N)   .. sycl::linearize2(gid1, lid1, T/N, N)+sycl::h::warp_sizes()]))));
+    context_everywhere (\forall int lid1=0 .. N, int gid1=0 .. T/N; ({:2:sycl::linearize2(gid1, lid1, T/N, N):}%4 == 1 ==> (0 <= lid1%sycl::h::warp_sizes() - 1 ==> lid1%sycl::h::warp_sizes()+sycl::h::warp_sizes()-1 <= sycl::h::warp_sizes() ==> sycl::linearize2(gid1, lid1, T/N, N)+sycl::h::warp_sizes()-1 <= |fyGs| ==>                             fx[sycl::linearize2(gid1, lid1, T/N, N)] == sum(fyGs[sycl::linearize2(gid1, lid1, T/N, N)-1 .. sycl::linearize2(gid1, lid1, T/N, N)+sycl::h::warp_sizes()-1]))));
+    context_everywhere (\forall int lid1=0 .. N, int gid1=0 .. T/N; ({:3:sycl::linearize2(gid1, lid1, T/N, N):}%4 == 2 ==> (0 <= lid1%sycl::h::warp_sizes() - 2 ==> lid1%sycl::h::warp_sizes()+sycl::h::warp_sizes()-2 <= sycl::h::warp_sizes() ==> sycl::linearize2(gid1, lid1, T/N, N)+sycl::h::warp_sizes()-2 <= |fzGs| ==> fx[sycl::linearize2(gid1, lid1, T/N, N)] == sum(fzGs[sycl::linearize2(gid1, lid1, T/N, N)-2 .. sycl::linearize2(gid1, lid1, T/N, N)+sycl::h::warp_sizes()-2]))));
     context_everywhere |fxGs| == Tf() && |fyGs| == Tf() && |fzGs| == Tf();
 
     ensures resultxx == sum(fxGs[0 .. Tf()]);
@@ -193,37 +196,37 @@ void accumulateResult(int T, int N, int* fx) {
         int lid = 0;
 
         /*@ loop_invariant 0 <= gid && gid < T/N;
-            loop_invariant 0 <= lid && lid <= N && lid%32==0;
+            loop_invariant 0 <= lid && lid <= N && lid%sycl::h::warp_sizes()==0;
             loop_invariant (lid < N ) ==> (0 <= sycl::linearize2(gid, lid, T/N, N) && sycl::linearize2(gid, lid, T/N, N) < Tf());
             
             loop_invariant (lid < N) ==> sycl::linearize2(gid, lid, T/N, N)%4 == 0;
             loop_invariant (lid < N ) ==> (resultx == sum(fxGs[0 .. sycl::linearize2(gid, lid, T/N, N)]) &&
                                            resulty == sum(fyGs[0 .. sycl::linearize2(gid, lid, T/N, N)]) &&
                                            resultz == sum(fzGs[0 .. sycl::linearize2(gid, lid, T/N, N)]));
-            loop_invariant (lid == N ) ==> (resultx == sum(fxGs[0 .. sycl::linearize2(gid, N-32, T/N, N)+32]) &&
-                                            resulty == sum(fyGs[0 .. sycl::linearize2(gid, N-32, T/N, N)+32]) &&
-                                            resultz == sum(fzGs[0 .. sycl::linearize2(gid, N-32, T/N, N)+32])); */
-        for (lid=0; lid < N; lid=lid+32){
+            loop_invariant (lid == N ) ==> (resultx == sum(fxGs[0 .. sycl::linearize2(gid, N-sycl::h::warp_sizes(), T/N, N)+sycl::h::warp_sizes()]) &&
+                                            resulty == sum(fyGs[0 .. sycl::linearize2(gid, N-sycl::h::warp_sizes(), T/N, N)+sycl::h::warp_sizes()]) &&
+                                            resultz == sum(fzGs[0 .. sycl::linearize2(gid, N-sycl::h::warp_sizes(), T/N, N)+sycl::h::warp_sizes()])); */
+        for (lid=0; lid < N; lid=lid+sycl::h::warp_sizes()){
             /*@ assert idshift(T,N,gid,lid); */
             //@ ghost int lidp1 = lid+1;
             //@ ghost int lidp2 = lid+2;
 
 
             resultx = resultx + fx[sycl::linearize2(gid, lid, T/N, N)];
-            /*@ assert lemmaSumOverConcat(fxGs[0 .. sycl::linearize2(gid, lid, T/N, N)],fxGs[sycl::linearize2(gid, lid, T/N, N) .. sycl::linearize2(gid, lid, T/N, N) + 32]);
-                assert lemmaSumOverABBCisAC(fxGs,0,sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lid, T/N, N)+32); */
+            /*@ assert lemmaSumOverConcat(fxGs[0 .. sycl::linearize2(gid, lid, T/N, N)],fxGs[sycl::linearize2(gid, lid, T/N, N) .. sycl::linearize2(gid, lid, T/N, N) + sycl::h::warp_sizes()]);
+                assert lemmaSumOverABBCisAC(fxGs,0,sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lid, T/N, N)+sycl::h::warp_sizes()); */
 
             resulty = resulty + fx[sycl::linearize2(gid, lidp1, T/N, N)];
-            /*@ assert lemmaSumOverConcat(fyGs[0 .. sycl::linearize2(gid, lid, T/N, N)],fyGs[sycl::linearize2(gid, lidp1, T/N, N)-1 .. sycl::linearize2(gid, lidp1, T/N, N)+32-1]);
-                assert lemmaSumOverABBCisAC(fyGs,0,sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lidp1, T/N, N)-1,sycl::linearize2(gid, lidp1, T/N, N)+32-1); */
+            /*@ assert lemmaSumOverConcat(fyGs[0 .. sycl::linearize2(gid, lid, T/N, N)],fyGs[sycl::linearize2(gid, lidp1, T/N, N)-1 .. sycl::linearize2(gid, lidp1, T/N, N)+sycl::h::warp_sizes()-1]);
+                assert lemmaSumOverABBCisAC(fyGs,0,sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lidp1, T/N, N)-1,sycl::linearize2(gid, lidp1, T/N, N)+sycl::h::warp_sizes()-1); */
 
             resultz = resultz + fx[sycl::linearize2(gid, lidp2, T/N, N)];
-            /*@ assert lemmaSumOverConcat(fzGs[0 .. sycl::linearize2(gid, lid, T/N, N)],fzGs[sycl::linearize2(gid, lidp2, T/N, N)-2 .. sycl::linearize2(gid, lidp2, T/N, N)+32-2]);
-                assert lemmaSumOverABBCisAC(fzGs,0,sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lidp2, T/N, N)-2,sycl::linearize2(gid, lidp2, T/N, N)+32-2); */
+            /*@ assert lemmaSumOverConcat(fzGs[0 .. sycl::linearize2(gid, lid, T/N, N)],fzGs[sycl::linearize2(gid, lidp2, T/N, N)-2 .. sycl::linearize2(gid, lidp2, T/N, N)+sycl::h::warp_sizes()-2]);
+                assert lemmaSumOverABBCisAC(fzGs,0,sycl::linearize2(gid, lid, T/N, N),sycl::linearize2(gid, lidp2, T/N, N)-2,sycl::linearize2(gid, lidp2, T/N, N)+sycl::h::warp_sizes()-2); */
 
-            //@     ghost int lid2=lid+32;
-            //@     assert (lid2 < N) ==> sycl::linearize2(gid, lid, T/N, N) + 32 == sycl::linearize2(gid, lid2, T/N, N);
-            //@     assert (lid2 == N) ==> sycl::linearize2(gid, lid, T/N, N) + 32 == sycl::linearize2(gid, lid2-32, T/N, N)+32;
+            //@     ghost int lid2=lid+sycl::h::warp_sizes();
+            //@     assert (lid2 < N) ==> sycl::linearize2(gid, lid, T/N, N) + sycl::h::warp_sizes() == sycl::linearize2(gid, lid2, T/N, N);
+            //@     assert (lid2 == N) ==> sycl::linearize2(gid, lid, T/N, N) + sycl::h::warp_sizes() == sycl::linearize2(gid, lid2-sycl::h::warp_sizes(), T/N, N)+sycl::h::warp_sizes();
         }
         //@ assert gidshift(T,N,gid);
     }
@@ -273,14 +276,14 @@ requires a <= b && c <= d && b == c;
 ensures \result;
 pure bool lemmaSumOverABBCisAC(seq<int> xs, int a, int b, int c, int d) = xs[a .. d] == xs[a .. b] + xs[c .. d];
 
-requires N > 0 && T > N && T%N == 0 && N%32==0 ;
+requires N > 0 && T > N && T%N == 0 && N%sycl::h::warp_sizes()==0 ;
 requires 0 <= gid && gid < T/N;
-requires 0 <= lid && lid < N && lid%32 == 0; 
+requires 0 <= lid && lid < N && lid%sycl::h::warp_sizes() == 0; 
 ensures \result;
-ensures sycl::linearize2(gid, lid, T/N, N)+32 <= T;
+ensures sycl::linearize2(gid, lid, T/N, N)+sycl::h::warp_sizes() <= T;
 pure bool idshift(int T, int N,int gid, int lid) = true; 
 
-requires N > 0 && T > N && T%N == 0 && N%32==0 ;
+requires N > 0 && T > N && T%N == 0 && N%sycl::h::warp_sizes()==0 ;
 requires 0 <= gid && gid < T/N;
 requires sycl::linearize2(gid, 0, T/N, N)%4 == 0;
 ensures gid+1<T/N  ==> sycl::linearize2(gid+1, 0, T/N, N)%4 == 0;
@@ -288,7 +291,7 @@ pure bool gidshift(int T, int N,int gid) = true;
 
 ensures \result >= 0;
 ensures N == 64 ==> \result == 6;
-ensures N == 32 ==> \result == 5;
+ensures N == sycl::h::warp_sizes() ==> \result == 5;
 ensures N == 16 ==> \result == 4;
 ensures sycl::h::exp(2,\result) == N;
 pure int logTwo(int N);
@@ -297,6 +300,6 @@ pure int logTwo(int N);
 /*
 pure bool NBound(int N) =
     (N == 16 && sycl::h::exp(2,4) == 16) ||
-    (N == 32 && sycl::h::exp(2,5) == 32) ||
+    (N == sycl::h::warp_sizes() && sycl::h::exp(2,5) == sycl::h::warp_sizes()) ||
     (N == 64 && sycl::h::exp(2,6) == 64);
 */
