1cc7e085229d2167c5ae50cdbfee5b5042db2057
mrskr <o.f.o.sakar@utwente.nl>
2026-06-24 14:12:08 +0200

    Added the new examples with Extract to the spec

--- test/main/vct/test/integration/examples/SYCLFullProgramsSpec.scala
+++ test/main/vct/test/integration/examples/SYCLFullProgramsSpec.scala
@@ -4,8 +4,12 @@ import vct.test.integration.helper.VercorsSpec
 
 class SYCLFullProgramsSpec extends VercorsSpec {
   vercors should verify using silicon flag
-    "--no-infer-heap-context-into-frame" example
+  "--no-infer-heap-context-into-frame" example
     "concepts/sycl/fullExamples/VectorAdd.cpp"
+
+  vercors should verify using silicon flag
+    "--no-infer-heap-context-into-frame" example
+    "concepts/sycl/fullExamples/VectorAddWithExtract.cpp"
 
   vercors should verify using silicon flags("--no-infer-heap-context-into-frame",
     "--prover-config=smt.arith.solver=6",
@@ -13,17 +17,25 @@ class SYCLFullProgramsSpec extends VercorsSpec {
     "--backend-option","--moreJoins=2") example
     "concepts/sycl/fullExamples/shiftFunctions/ShiftGroupLeft.cpp"
 
-  // The example verifies, but gets stuck once in a while so I'm disabling it. The same for the Helper file.
+  vercors should verify using silicon flags("--no-infer-heap-context-into-frame",
+    "--prover-config=smt.arith.solver=6",
+    "--backend-option", "--numberOfErrorsToReport=0",
+    "--backend-option", "--moreJoins=2") example
+    "concepts/sycl/fullExamples/MatrixTransposeWithF.cpp"
+
+  vercors should verify using silicon flags("--no-infer-heap-context-into-frame",
+    "--prover-config=smt.arith.solver=6",
+    "--backend-option", "--numberOfErrorsToReport=0",
+    "--backend-option", "--moreJoins=2") example
+    "concepts/sycl/fullExamples/MatrixTransposeWithFWithExtract.cpp"
+
+}
+
+
+// The example verifies, but gets stuck once in a while so I'm disabling it. The same for the Helper file.
 //  vercors should verify using silicon flags("--no-infer-heap-context-into-frame",
 //    "--prover-config=smt.arith.solver=6",
 //    "--backend-option", "--numberOfErrorsToReport=0",
 //    "--backend-option", "--moreJoins=2") example
 //    "concepts/sycl/kernels/ShiftGroupSum.cpp"
 
-
-   vercors should verify using silicon flags("--no-infer-heap-context-into-frame",
-    "--prover-config=smt.arith.solver=6",
-    "--backend-option", "--numberOfErrorsToReport=0",
-    "--backend-option", "--moreJoins=2") example
-     "concepts/sycl/fullExamples/MatrixTransposeWithF.cpp"
-}
