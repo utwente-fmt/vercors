54968fb5210185b7a64759630b0a1cf6cb1d1748
mrskr <o.f.o.sakar@utwente.nl>
2026-08-24 16:46:21 +0200

    Added the ReentLock example

--- test/main/vct/test/integration/examples/AtomicsSpec.scala
+++ test/main/vct/test/integration/examples/AtomicsSpec.scala
@@ -10,5 +10,6 @@ class AtomicsSpec extends VercorsSpec {
   vercors should verify using silicon example "concepts/atomics/ReentractLock.java"
   vercors should verify using silicon example "concepts/atomics/Semaphore.java"
   vercors should verify using silicon example "concepts/atomics/AtomicReadWrite.java"
+  vercors should verify using silicon example "concepts/atomics/ReentLock.java"
 
 }
