8e7f84d32c9ed6f7690545efe390b9bffc2dc973
mrskr <o.f.o.sakar@utwente.nl>
2026-09-01 16:26:54 +0200

    Ordered AtomicsSpec, for some reason the CI doesnt see that this spec exists?

--- test/main/vct/test/integration/examples/AtomicsSpec.scala
+++ test/main/vct/test/integration/examples/AtomicsSpec.scala
@@ -3,14 +3,14 @@ package vct.test.integration.examples
 import vct.test.integration.helper.VercorsSpec
 
 class AtomicsSpec extends VercorsSpec {
-  vercors should verify using silicon example "concepts/atomics/SingleCell.java"
-  vercors should verify using silicon example "concepts/atomics/RBProdCons.java"
-  vercors should verify using silicon example "concepts/atomics/RBLock.java"
+  vercors should verify using silicon example "concepts/atomics/AtomicReadWrite.java"
   vercors should verify using silicon example "concepts/atomics/CountDownLatch.java"
+  vercors should verify using silicon example "concepts/atomics/RBLock.java"
+  vercors should verify using silicon example "concepts/atomics/RBProdCons.java"
+  vercors should verify using silicon example "concepts/atomics/RBSingleCell.java"
+  vercors should verify using silicon example "concepts/atomics/ReentLock.java"
   vercors should verify using silicon example "concepts/atomics/ReentrantLock.java"
   vercors should verify using silicon example "concepts/atomics/Semaphore.java"
-  vercors should verify using silicon example "concepts/atomics/AtomicReadWrite.java"
-  vercors should verify using silicon example "concepts/atomics/ReentLock.java"
-  vercors should verify using silicon example "concepts/atomics/RBSingleCell.java"
+  vercors should verify using silicon example "concepts/atomics/SingleCell.java"
 
 }
